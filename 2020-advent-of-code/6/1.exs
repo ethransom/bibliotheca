8c13e12a8e746e355830bf7d5b8925a7e75f308f
Ethan Ransom <derethanhausen@gmail.com>
2020-12-06 23:24:38 -0700

    AoC 2020: minor cleanups day 6

--- 2020-advent-of-code/6/1.exs
+++ 2020-advent-of-code/6/1.exs
@@ -3,12 +3,10 @@
 input
 |> String.split("\n\n")
 |> Enum.map(fn group ->
-  String.split(group, "\n")
-  |> Enum.map(fn line ->
-    line |> String.graphemes() |> Enum.map(& {&1, 0})
-  end)
-  |> List.flatten()
-  |> Map.new()
+  group
+  |> String.graphemes()
+  |> Enum.filter(& &1 != "\n")
+  |> Enum.uniq()
   |> Enum.count()
 end)
 |> Enum.sum()
