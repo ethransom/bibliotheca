a8dec5e19a22966ba9780f421ef920860c4bc132
Ethan Ransom <derethanhausen@gmail.com>
2021-11-29 22:14:50 -0700

    fastrtlamr: rearrange

--- power-meters/fastrtlamr/src/main.rs
+++ power-meters/fastrtlamr/src/main.rs
@@ -3,30 +3,15 @@ use std::io::Read;
 use std::net::TcpStream;
 use std::str::from_utf8;
 
-// /// Read the stream data and return stream data & its length
-// fn read_stream(stream: &mut TcpStream) -> (Vec<u8>, usize) {
-//     let buffer_size = 512;
-//     let mut request_buffer = vec![];
-//     // let us loop & try to read the whole request data
-//     let mut request_len = 0usize;
-//     loop {
-//         let mut buffer = vec![0; buffer_size];
-//         match stream.read(&mut buffer) {
-//             Ok(n) => {
-//                 if n == 0 {
-//                     break;
-//                 } else {
-//                     request_len += n;
-//                 }
-//             Err(e) {
-//                 println!("Error in reading stream data: {:?}", e);
-//                 break;
-//             }
-//         }
-//     }
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
 
-//     (request_buffer, request_len)
-// }
+    let default_addr = String::from("localhost:1234");
+    let addr = args.get(1).unwrap_or(&default_addr);
+
+    open_stream(addr).unwrap_or_else(|e| panic!("connection error: {}", e));
+}
+
 const RTLTCP_MAGIC_NUM: &[u8; 4] = b"RTL0";
 
 fn open_stream(addr: &str) -> Result<TcpStream, String> {
@@ -65,13 +50,4 @@ fn tuner_display_name(tuner: u32) -> &'static str {
         6 => "R828D",
         _ => "UNKNOWN",
     }
-}
-
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    let default_addr = String::from("localhost:1234");
-    let addr = args.get(1).unwrap_or(&default_addr);
-
-    open_stream(addr).unwrap_or_else(|e| panic!("connection error: {}", e));
 }
