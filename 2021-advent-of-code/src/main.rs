24e16c63942843e2f41015bb0d223d5e73acc1a1
Ethan Ransom <derethanhausen@gmail.com>
2021-11-30 22:47:15 -0700

    AoC 2021: day 1 parts 1&2: initial solution

--- 2021-advent-of-code/src/main.rs
+++ 2021-advent-of-code/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let dat = std::str::from_utf8(include_bytes!("input.txt")).expect("bad input file!");
+
+    let nums: Vec<u64> = dat
+        .lines()
+        .map(|line| line.parse::<u64>().unwrap())
+        .collect();
+
+    let mut count = 0;
+    for i in 1..nums.len() {
+        if nums[i] > nums[i - 1] {
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
+
+    let mut sums: Vec<u64> = Vec::new();
+
+    for i in 0..nums.len() - 2 {
+        sums.push(nums[i] + nums[i + 1] + nums[i + 2]);
+    }
+
+    let mut count = 0;
+    for i in 1..sums.len() {
+        if sums[i] > sums[i - 1] {
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
 }
