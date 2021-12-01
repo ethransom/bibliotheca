6e939748e1eadf18e5f9ab107044a8e3410cad32
Ethan Ransom <derethanhausen@gmail.com>
2021-11-30 23:57:26 -0700

    AoC 2021: day 1: write alt soln with windowed iterators (it's faster???!)

--- 2021-advent-of-code/src/bin/part01.rs
+++ 2021-advent-of-code/src/bin/part01.rs
@@ -5,9 +5,13 @@ const INPUT: &[u8] = include_bytes!("input01.txt");
 
 fn main() {
     let example = parse(EXAMPLE);
-    println!("example: {:?}", solve(&example));
+    println!("example:");
+    println!("\t{:?}", solve(&example));
+    println!("\t{:?}", solve_windowed(&example));
     let input = parse(INPUT);
-    println!("input: {:?}", solve(&input));
+    println!("input:");
+    println!("\t{:?}", solve(&input));
+    println!("\t{:?}", solve_windowed(&input));
 }
 
 fn parse(dat: &[u8]) -> Vec<u64> {
@@ -18,7 +22,7 @@ fn parse(dat: &[u8]) -> Vec<u64> {
         .collect()
 }
 
-fn solve(nums: &Vec<u64>) -> (u64, u64) {
+pub fn solve(nums: &Vec<u64>) -> (u64, u64) {
     let mut simple_increases = 0;
     for i in 1..nums.len() {
         if nums[i] > nums[i - 1] {
@@ -38,6 +42,20 @@ fn solve(nums: &Vec<u64>) -> (u64, u64) {
             windowed_increases += 1;
         }
     }
+
+    (simple_increases, windowed_increases)
+}
+
+pub fn solve_windowed(nums: &Vec<u64>) -> (usize, usize) {
+    let simple_increases = nums.windows(2).filter(|w| w[1] > w[0]).count();
+
+    let windowed_increases = nums
+        .windows(3)
+        .map(|w| w[0] + w[1] + w[2])
+        .collect::<Vec<u64>>()
+        .windows(2)
+        .filter(|w| w[1] > w[0])
+        .count();
 
     (simple_increases, windowed_increases)
 }
@@ -86,5 +104,14 @@ fn bench_solve(b: &mut test::Bencher) {
 
     b.iter(|| {
         solve(&input);
+    });
+}
+
+#[bench]
+fn bench_solve_windowed(b: &mut test::Bencher) {
+    let input = parse(INPUT);
+
+    b.iter(|| {
+        solve_windowed(&input);
     });
 }
