3e67ca137cc575b58b5264356005aa918a0787f9
Ethan Ransom <derethanhausen@gmail.com>
2021-12-05 19:08:25 -0700

    aoc21: 4: rearrange benchmarks massively

--- 2021-advent-of-code/src/bin/day04.rs
+++ 2021-advent-of-code/src/bin/day04.rs
@@ -12,6 +12,64 @@ fn main() {
 
 type Board = [[u8; 5]; 5];
 
+#[derive(Clone, Debug)]
+struct MemoryBoard {
+    board: Board,
+    turns: usize, // only used for 'memoized' and following benchmarks
+    marked: [[bool; 5]; 5],
+}
+
+impl MemoryBoard {
+    fn new(board: &Board) -> MemoryBoard {
+        MemoryBoard {
+            board: *board,
+            turns: 0,
+            marked: [[false; 5]; 5],
+        }
+    }
+    fn mark(&mut self, draw: u8) {
+        self.turns += 1;
+        for row in 0..5 {
+            for col in 0..5 {
+                if self.board[row][col] == draw {
+                    self.marked[row][col] = true;
+                }
+            }
+        }
+    }
+    fn has_won(&self) -> bool {
+        'nextrow: for row in self.marked {
+            for cell in row {
+                if cell == false {
+                    continue 'nextrow;
+                }
+            }
+            return true;
+        }
+        'nextcol: for col in 0..5 {
+            for row in self.marked {
+                if row[col] == false {
+                    continue 'nextcol;
+                }
+            }
+            return true;
+        }
+
+        false
+    }
+    fn score(&self, drawn: &[u8]) -> usize {
+        let mut sum: usize = 0;
+        for row in self.board {
+            for cell in row {
+                if let None = drawn.iter().position(|r| *r == cell) {
+                    sum += cell as usize;
+                }
+            }
+        }
+        sum
+    }
+}
+
 fn parse(input: &[u8]) -> (Vec<u8>, Vec<Board>) {
     let blocks = std::str::from_utf8(input)
         .expect("input was not utf8")
@@ -40,84 +98,38 @@ fn parse(input: &[u8]) -> (Vec<u8>, Vec<Board>) {
 }
 
 fn solve(input: &[u8]) -> (usize, usize) {
-    let (numbers, boards) = parse(input);
-
-    let mut wins_on: Vec<Option<usize>> = vec![None; boards.len()];
-    for turn in 1..numbers.len() {
-        let drawn = &numbers[0..turn];
-
-        for (board_num, board) in boards.iter().enumerate() {
-            if matches!(wins_on[board_num], None) && has_won(board, drawn) {
-                wins_on[board_num] = Some(turn);
-            }
-        }
-    }
-
-    // do we really need this check? puzzle input seems to always satisfy this...
-    let wins_on: Vec<usize> = wins_on
-        .into_iter()
-        .map(|win| {
-            if let Some(win) = win {
-                win
-            } else {
-                panic!("not all boards won!")
-            }
-        })
+    let (numbers, raw_boards) = parse(input);
+    let boards: Vec<MemoryBoard> = raw_boards
+        .iter()
+        .map(|board| MemoryBoard::new(board))
         .collect();
 
-    let (board_no, turn) = wins_on
+    let mut boards: Vec<MemoryBoard> = boards.clone();
+    for draw in numbers.iter() {
+        for board in boards.iter_mut() {
+            if !board.has_won() {
+                board.mark(*draw);
+            }
+        }
+    }
+
+    let max_board = boards
         .iter()
-        .enumerate()
-        .max_by(|(_, turn_a), (_, turn_b)| turn_a.cmp(turn_b))
+        .max_by(|board_a, board_b| board_a.turns.cmp(&board_b.turns))
         .expect("no wins");
 
-    let first_score = score(&boards[board_no], &numbers[0..*turn]) * numbers[turn - 1] as usize;
+    let turn = max_board.turns;
+    let first_score = max_board.score(&numbers[0..turn]) * numbers[turn - 1] as usize;
 
-    let (board_no, turn) = wins_on
+    let min_board = boards
         .iter()
-        .enumerate()
-        .min_by(|(_, turn_a), (_, turn_b)| turn_a.cmp(turn_b))
+        .min_by(|board_a, board_b| board_a.turns.cmp(&board_b.turns))
         .expect("no wins");
 
-    let last_score = score(&boards[board_no], &numbers[0..*turn]) * numbers[turn - 1] as usize;
+    let turn = min_board.turns;
+    let last_score = min_board.score(&numbers[0..turn]) * numbers[turn - 1] as usize;
 
     (first_score, last_score)
-}
-
-fn has_won(board: &Board, drawn: &[u8]) -> bool {
-    'nextrow: for row in board {
-        for cell in row {
-            if let None = drawn.iter().position(|&r| r == *cell) {
-                continue 'nextrow;
-            }
-        }
-
-        return true;
-    }
-
-    'nextcol: for col in 0..5 {
-        for row in board {
-            if let None = drawn.iter().position(|&r| r == row[col]) {
-                continue 'nextcol;
-            }
-        }
-
-        return true;
-    }
-
-    false
-}
-
-fn score(board: &Board, drawn: &[u8]) -> usize {
-    let mut sum: usize = 0;
-    for row in board {
-        for cell in row {
-            if let None = drawn.iter().position(|r| *r == *cell) {
-                sum += *cell as usize;
-            }
-        }
-    }
-    sum
 }
 
 #[test]
@@ -170,9 +182,12 @@ fn bench_parse_01_next(b: &mut test::Bencher) {
 }
 
 #[bench]
-fn bench_compute_wins_00_original(b: &mut test::Bencher) {
-    let (numbers, boards) = parse(INPUT);
-    b.iter(|| {
+fn bench_solve_00_original(b: &mut test::Bencher) {
+    // original solution, reproduced here for posterity
+
+    fn solve(input: &[u8]) -> (usize, usize) {
+        let (numbers, boards) = parse(input);
+
         let mut wins_on: Vec<Option<usize>> = vec![None; boards.len()];
         for turn in 1..numbers.len() {
             let drawn = &numbers[0..turn];
@@ -183,14 +198,93 @@ fn bench_compute_wins_00_original(b: &mut test::Bencher) {
                 }
             }
         }
-        wins_on
-    });
+
+        // do we really need this check? puzzle input seems to always satisfy this...
+        let wins_on: Vec<usize> = wins_on
+            .into_iter()
+            .map(|win| {
+                if let Some(win) = win {
+                    win
+                } else {
+                    panic!("not all boards won!")
+                }
+            })
+            .collect();
+
+        let (board_no, turn) = wins_on
+            .iter()
+            .enumerate()
+            .max_by(|(_, turn_a), (_, turn_b)| turn_a.cmp(turn_b))
+            .expect("no wins");
+
+        let first_score = score(&boards[board_no], &numbers[0..*turn]) * numbers[turn - 1] as usize;
+
+        let (board_no, turn) = wins_on
+            .iter()
+            .enumerate()
+            .min_by(|(_, turn_a), (_, turn_b)| turn_a.cmp(turn_b))
+            .expect("no wins");
+
+        let last_score = score(&boards[board_no], &numbers[0..*turn]) * numbers[turn - 1] as usize;
+
+        (first_score, last_score)
+    }
+
+    fn has_won(board: &Board, drawn: &[u8]) -> bool {
+        'nextrow: for row in board {
+            for cell in row {
+                if let None = drawn.iter().position(|&r| r == *cell) {
+                    continue 'nextrow;
+                }
+            }
+
+            return true;
+        }
+
+        'nextcol: for col in 0..5 {
+            for row in board {
+                if let None = drawn.iter().position(|&r| r == row[col]) {
+                    continue 'nextcol;
+                }
+            }
+
+            return true;
+        }
+
+        false
+    }
+
+    fn score(board: &Board, drawn: &[u8]) -> usize {
+        let mut sum: usize = 0;
+        for row in board {
+            for cell in row {
+                if let None = drawn.iter().position(|r| *r == *cell) {
+                    sum += *cell as usize;
+                }
+            }
+        }
+        sum
+    }
+
+    b.iter(|| assert_eq!(solve(INPUT), (8468, 39984)));
 }
 
 #[bench]
 // Hilariously, this is NOT faster.
 // Maybe we're paying too much to sort, maybe real bottleneck is scanning the cells.
-fn bench_compute_wins_01_binary(b: &mut test::Bencher) {
+fn bench_solve_01_binary(b: &mut test::Bencher) {
+    fn score(board: &Board, drawn: &[u8]) -> usize {
+        let mut sum: usize = 0;
+        for row in board {
+            for cell in row {
+                if let None = drawn.iter().position(|r| *r == *cell) {
+                    sum += *cell as usize;
+                }
+            }
+        }
+        sum
+    }
+
     fn has_won(board: &Board, drawn: &[u8]) -> bool {
         'nextrow: for row in board {
             for cell in row {
@@ -214,8 +308,10 @@ fn bench_compute_wins_01_binary(b: &mut test::Bencher) {
 
         false
     }
-    let (numbers, boards) = parse(INPUT);
+
     b.iter(|| {
+        let (numbers, boards) = parse(INPUT);
+
         // `numbers` would be needed for scoring later on
         let mut lookup = numbers.clone();
         let mut wins_on: Vec<Option<usize>> = vec![None; boards.len()];
@@ -263,66 +359,13 @@ fn bench_compute_wins_01_binary(b: &mut test::Bencher) {
 }
 
 #[bench]
-fn bench_compute_wins_02_memory(b: &mut test::Bencher) {
-    #[derive(Clone, Debug)]
-    struct MemoryBoard {
-        board: Board,
-        marked: [[bool; 5]; 5],
-    }
-    impl MemoryBoard {
-        fn new(board: &Board) -> MemoryBoard {
-            MemoryBoard {
-                board: *board,
-                marked: [[false; 5]; 5],
-            }
-        }
-        fn mark(&mut self, draw: u8) {
-            for row in 0..5 {
-                for col in 0..5 {
-                    if self.board[row][col] == draw {
-                        self.marked[row][col] = true;
-                    }
-                }
-            }
-        }
-        fn has_won(&self) -> bool {
-            'nextrow: for row in self.marked {
-                for cell in row {
-                    if cell == false {
-                        continue 'nextrow;
-                    }
-                }
-                return true;
-            }
-            'nextcol: for col in 0..5 {
-                for row in self.marked {
-                    if row[col] == false {
-                        continue 'nextcol;
-                    }
-                }
-                return true;
-            }
-
-            false
-        }
-        fn score(&self, drawn: &[u8]) -> usize {
-            let mut sum: usize = 0;
-            for row in self.board {
-                for cell in row {
-                    if let None = drawn.iter().position(|r| *r == cell) {
-                        sum += cell as usize;
-                    }
-                }
-            }
-            sum
-        }
-    }
-    let (numbers, raw_boards) = parse(INPUT);
-    let boards: Vec<MemoryBoard> = raw_boards
-        .iter()
-        .map(|board| MemoryBoard::new(board))
-        .collect();
+fn bench_solve_02_memory(b: &mut test::Bencher) {
     b.iter(|| {
+        let (numbers, raw_boards) = parse(INPUT);
+        let boards: Vec<MemoryBoard> = raw_boards
+            .iter()
+            .map(|board| MemoryBoard::new(board))
+            .collect();
         let mut boards: Vec<MemoryBoard> = boards.clone();
         let mut wins_on: Vec<Option<usize>> = vec![None; boards.len()];
         for (i, draw) in numbers.iter().enumerate() {
@@ -366,4 +409,44 @@ fn bench_compute_wins_02_memory(b: &mut test::Bencher) {
 
         assert_eq!((first_score, last_score), (8468, 39984))
     });
+}
+
+#[bench]
+fn bench_solve_03_memory_memoize_turns(b: &mut test::Bencher) {
+    fn solve(input: &[u8]) -> (usize, usize) {
+        let (numbers, raw_boards) = parse(input);
+        let boards: Vec<MemoryBoard> = raw_boards
+            .iter()
+            .map(|board| MemoryBoard::new(board))
+            .collect();
+
+        let mut boards: Vec<MemoryBoard> = boards.clone();
+        for draw in numbers.iter() {
+            for board in boards.iter_mut() {
+                if !board.has_won() {
+                    board.mark(*draw);
+                }
+            }
+        }
+
+        let max_board = boards
+            .iter()
+            .max_by(|board_a, board_b| board_a.turns.cmp(&board_b.turns))
+            .expect("no wins");
+
+        let turn = max_board.turns;
+        let first_score = max_board.score(&numbers[0..turn]) * numbers[turn - 1] as usize;
+
+        let min_board = boards
+            .iter()
+            .min_by(|board_a, board_b| board_a.turns.cmp(&board_b.turns))
+            .expect("no wins");
+
+        let turn = min_board.turns;
+        let last_score = min_board.score(&numbers[0..turn]) * numbers[turn - 1] as usize;
+
+        (first_score, last_score)
+    }
+
+    b.iter(|| assert_eq!(solve(INPUT), (8468, 39984)));
 }
