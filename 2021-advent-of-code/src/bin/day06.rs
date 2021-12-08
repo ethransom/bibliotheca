6440b6b6f74952cd757b52a7fb7b23e39922da16
Ethan Ransom <derethanhausen@gmail.com>
2021-12-07 18:32:06 -0700

    aoc21: 6: use include_str! and cleanup benches

--- 2021-advent-of-code/src/bin/day06.rs
+++ 2021-advent-of-code/src/bin/day06.rs
@@ -2,8 +2,8 @@
 
 extern crate test;
 
-const EXAMPLE: &[u8] = include_bytes!("example06.txt");
-const INPUT: &[u8] = include_bytes!("input06.txt");
+const EXAMPLE: &str = include_str!("example06.txt");
+const INPUT: &str = include_str!("input06.txt");
 
 fn main() {
     dbg!(solve(EXAMPLE));
@@ -17,9 +17,7 @@ fn simulate(initial: &Vec<u8>, rounds: usize) -> usize {
         counts[fish as usize] += 1;
     }
 
-    println!("initial: {:?}", counts);
-
-    for i in 0..rounds {
+    for _round in 0..rounds {
         let breeders = counts[0];
 
         // shift down
@@ -30,33 +28,116 @@ fn simulate(initial: &Vec<u8>, rounds: usize) -> usize {
         counts[6] += breeders;
 
         counts[8] = breeders;
-
-        println!("After day {}: {}", i + 1, counts.iter().sum::<usize>());
     }
 
     return counts.iter().sum();
 }
 
-fn solve(input: &[u8]) -> (usize, usize) {
-    let fishes = std::str::from_utf8(input)
-        .expect("input was not utf8")
+fn solve(input: &str) -> (usize, usize) {
+    let fishes = input
         .split(",")
         .map(|n| n.parse().expect("not a number"))
         .collect::<Vec<u8>>();
-
-    println!("Initial state: {}", fishes.len());
 
     (simulate(&fishes, 80), simulate(&fishes, 256))
 }
 
 #[test]
 fn test_example() {
-    assert_eq!(solve(EXAMPLE), (5934, 26_984_457_539));
+    assert_eq!(solve(EXAMPLE), (5_934, 26_984_457_539));
 }
 
 #[bench]
-fn bench_solve_current(b: &mut test::Bencher) {
+fn bench_solve_00_original(b: &mut test::Bencher) {
     b.iter(|| {
         assert_eq!(solve(INPUT), (354_564, 1_609_058_859_115));
     });
 }
+
+#[bench]
+fn bench_solve_01_shift(b: &mut test::Bencher) {
+    fn simulate(initial: &Vec<u8>, rounds: usize) -> usize {
+        let mut counts = vec![0 as usize; 9];
+
+        for &fish in initial {
+            counts[fish as usize] += 1;
+        }
+
+        for _round in 0..rounds {
+            let breeders = counts[0];
+
+            counts.rotate_left(1);
+
+            counts[6] += breeders;
+
+            counts[8] = breeders;
+        }
+
+        return counts.iter().sum();
+    }
+
+    fn solve(input: &str) -> (usize, usize) {
+        let fishes = input
+            .split(",")
+            .map(|n| n.parse().expect("not a number"))
+            .collect::<Vec<u8>>();
+
+        (simulate(&fishes, 80), simulate(&fishes, 256))
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (354_564, 1_609_058_859_115));
+    });
+}
+
+#[bench]
+fn bench_solve_02_stack_alloc(b: &mut test::Bencher) {
+    fn simulate(mut counts: [usize; 9], results: &mut [usize]) {
+        for round in 0..results.len() {
+            let breeders = counts[0];
+            // shift down
+            for i in 1..9 {
+                counts[i - 1] = counts[i];
+            }
+            counts[6] += breeders;
+            counts[8] = breeders;
+
+            results[round] = counts.iter().sum();
+        }
+    }
+
+    fn solve(input: &str) -> (usize, usize) {
+        let mut initial = [0 as usize; 9];
+        input
+            .split(",")
+            .for_each(|n| initial[n.parse::<usize>().expect("not a number")] += 1);
+
+        let mut results = [0 as usize; 256];
+
+        simulate(initial, &mut results);
+
+        (results[80 - 1], results[256 - 1])
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (354_564, 1_609_058_859_115));
+    });
+}
+
+// TODO: could we leave uints in the count vec in place and rotate
+//       some pointer or index around the vec? something like:
+// ```
+// fn simulate(mut counts: [usize; 6], results: &mut [usize]) {
+//     let mut young = [0 as usize; 2];
+
+//     for round in 0..results.len() {
+//         let breeders = counts[round % 6];
+
+//         counts[round % 6] += young[round % 2];
+
+//         young[round % 2] = breeders;
+
+//         results[round] = counts.iter().sum::<usize>() + young[0] + young[1];
+//     }
+// }
+// ```
