aadd1675a9932631754cf3cef1517705ac4b8a13
Ethan Ransom <derethanhausen@gmail.com>
2021-12-18 19:04:32 -0700

    aoc21: 18: explode pairs when deeper than 4 levels

--- 2021-advent-of-code/src/bin/day18.rs
+++ 2021-advent-of-code/src/bin/day18.rs
@@ -11,28 +11,42 @@ fn main() {
 }
 
 fn solve(input: &str) -> (u32, u32) {
-    let numbers: Vec<Pair> = input.lines().map(Pair::from).collect();
-
-    let result = numbers
-        .into_iter()
+    let result = input
+        .lines()
+        .map(Pair::from)
         .reduce(|a, b| a + b)
         .expect("not enough numbers to add");
 
     (result.magnitude(), 0)
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq)]
 struct Pair {
     left: Arm,
     right: Arm,
 }
 
-#[derive(PartialEq, Debug)]
+impl std::fmt::Debug for Pair {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{:?},{:?}]", self.left, self.right)
+    }
+}
+
+#[derive(PartialEq)]
 enum Arm {
     Num(u32),
     Branch(Box<Pair>),
 }
 use Arm::{Branch, Num};
+
+impl std::fmt::Debug for Arm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Num(n) => write!(f, "{}", n),
+            Branch(b) => write!(f, "{:?}", b),
+        }
+    }
+}
 
 impl From<&str> for Pair {
     fn from(line: &str) -> Pair {
@@ -166,7 +180,7 @@ impl std::ops::Add<Pair> for Pair {
             left: Branch(Box::new(self)),
             right: Branch(Box::new(other)),
         }
-        // TODO: .reduce()
+        .reduce()
     }
 }
 
@@ -187,8 +201,208 @@ fn test_add() {
         Pair::from("[[[[1,1],[2,2]],[3,3]],[4,4]]")
     );
 
+    assert_eq!(
+        Pair::from("[[[[4,3],4],4],[7,[[8,4],9]]]") + Pair::from("[1,1]"),
+        Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+    );
+}
+
+impl Pair {
+    fn reduce(self) -> Pair {
+        let mut pair = self;
+        pair.explode(0);
+
+        pair
+    }
+
+    fn explode(&mut self, depth: usize) -> Option<(u32, u32)> {
+        let left_carry = if depth + 1 == 4 {
+            let carry = if let Branch(pair) = &mut self.left {
+                if let Num(left) = pair.left {
+                    if let Num(right) = pair.right {
+                        Some((left, right))
+                    } else {
+                        panic!("tried to explode pair with children on right branch");
+                    }
+                } else {
+                    panic!("tried to explode pair with children on left branch");
+                }
+            } else {
+                None
+            };
+
+            if carry.is_some() {
+                self.left = Num(0);
+            }
+
+            carry
+        } else if let Branch(pair) = &mut self.left {
+            let carry = pair.explode(depth + 1);
+            println!(
+                "returned carry from left  ({:?}) of {:?}",
+                &self.left, carry
+            );
+            carry
+        } else {
+            None
+        };
+
+        if let Some(left_carry) = left_carry {
+            let (left_left_carry, left_right_carry) = left_carry;
+
+            if let Num(right) = self.right {
+                if left_right_carry != 0 {
+                    self.right = Num(right + left_right_carry);
+                }
+                return Some((left_left_carry, 0));
+            }
+            // right is of Branch
+            if left_right_carry != 0 {
+                let mut placement = &mut self.right;
+                loop {
+                    match placement {
+                        Num(right) => {
+                            println!("placing {} in {}", left_right_carry, right);
+                            *placement = Num(left_right_carry + *right);
+                            break;
+                        }
+                        Branch(left) => placement = &mut left.left,
+                    }
+                }
+                return Some((left_left_carry, 0));
+            }
+
+            return Some(left_carry);
+        }
+
+        let right_carry = if depth + 1 == 4 {
+            let carry = if let Branch(pair) = &mut self.right {
+                if let Num(left) = pair.left {
+                    if let Num(right) = pair.right {
+                        Some((left, right))
+                    } else {
+                        panic!("tried to explode pair with children on right branch");
+                    }
+                } else {
+                    panic!("tried to explode pair with children on left branch");
+                }
+            } else {
+                None
+            };
+
+            if carry.is_some() {
+                self.right = Num(0);
+            }
+
+            carry
+        } else if let Branch(pair) = &mut self.right {
+            let carry = pair.explode(depth + 1);
+            println!(
+                "returned carry from right ({:?})  of {:?}",
+                &self.right, carry
+            );
+            carry
+        } else {
+            None
+        };
+
+        if let Some(right_carry) = right_carry {
+            let (right_left_carry, right_right_carry) = right_carry;
+
+            if let Num(left) = self.left {
+                if right_left_carry != 0 {
+                    self.left = Num(left + right_left_carry);
+                }
+                return Some((0, right_right_carry));
+            }
+            // right is of Branch
+            if right_left_carry != 0 {
+                let mut placement = &mut self.left;
+                loop {
+                    match placement {
+                        Num(left) => {
+                            println!("placing {} in {}", right_left_carry, left);
+                            *placement = Num(right_left_carry + *left);
+                            break;
+                        }
+                        Branch(right) => placement = &mut right.right,
+                    }
+                }
+                return Some((0, right_right_carry));
+            }
+
+            return Some(right_carry);
+        }
+
+        right_carry
+    }
+}
+
+#[test]
+fn test_reduce_explode() {
+    // reduced numbers do not explode
+    assert_eq!(
+        Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]").reduce(),
+        Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+    );
+
+    // SIMPLE EXPLODES
+    // the 9 has no regular number to its left, so it is not added to any regular number
+    assert_eq!(
+        Pair::from("[[[[[9,8],1],2],3],4]").reduce(),
+        Pair::from("[[[[0,9],2],3],4]")
+    );
+    // the 2 has no regular number to its right, and so it is not added to any regular number
+    assert_eq!(
+        Pair::from("[7,[6,[5,[4,[3,2]]]]]").reduce(),
+        Pair::from("[7,[6,[5,[7,0]]]]")
+    );
+    assert_eq!(
+        Pair::from("[[6,[5,[4,[3,2]]]],1]").reduce(),
+        Pair::from("[[6,[5,[7,0]]],3]")
+    );
+    // the pair [3,2] is unaffected because the pair [7,3] is further to the left; [3,2] would explode on the next action
+    assert_eq!(
+        Pair::from("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]").reduce(),
+        Pair::from("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+    );
+    assert_eq!(
+        Pair::from("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]").reduce(),
+        Pair::from("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+    );
+}
+
+#[test]
+fn test_reduce_example() {
+    // reduced numbers are reduced
+    assert_eq!(
+        Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]").reduce(),
+        Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+    );
+    // explode
+    assert_eq!(
+        Pair::from("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]").reduce(),
+        Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+    );
+    // // split
     // assert_eq!(
-    //     Pair::from("[[[[4,3],4],4],[7,[[8,4],9]]]") + Pair::from("[1,1]"),
+    //     Pair::from("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]").reduce(),
+    //     Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+    // );
+    // // split
+    // assert_eq!(
+    //     Pair::from("[[[[0,7],4],[15,[0,13]]],[1,1]]").reduce(),
+    //     Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+    // );
+    // // explode
+    // assert_eq!(
+    //     Pair::from("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]").reduce(),
+    //     Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+    // );
+    // // explode
+    // // NOTE: same as addition example
+    // assert_eq!(
+    //     Pair::from("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]").reduce(),
     //     Pair::from("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
     // );
 }
