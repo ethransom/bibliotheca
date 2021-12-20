432caace58b0179a66310f157133d9b10f765a7b
Ethan Ransom <derethanhausen@gmail.com>
2021-12-19 22:29:28 -0700

    aoc21: 19: parse

---
+++ 2021-advent-of-code/src/bin/day19.rs
@@ -0,0 +1,64 @@
+#![feature(test)]
+
+extern crate test;
+
+const EXAMPLE: &str = include_str!("example19.txt");
+const INPUT: &str = include_str!("input19.txt");
+
+fn main() {
+    dbg!(solve(EXAMPLE));
+    dbg!(solve(INPUT));
+}
+
+fn solve(input: &str) -> (usize, usize) {
+    let blocks: Vec<Vec<(i32, i32, i32)>> = input
+        .split("\n\n")
+        .map(|scanner| {
+            let mut lines = scanner.lines();
+
+            lines.next().expect("expected new scanner block");
+
+            lines
+                .map(|lines| {
+                    let mut coords = lines.split(',');
+
+                    (
+                        coords
+                            .next()
+                            .expect("expected x coord")
+                            .parse::<i32>()
+                            .expect("x coord not a number"),
+                        coords
+                            .next()
+                            .expect("expected y coord")
+                            .parse::<i32>()
+                            .expect("y coord not a number"),
+                        if let Some(z) = coords.next() {
+                            z.parse::<i32>().expect("z coord not a number")
+                        } else {
+                            0
+                        }
+                    )
+                })
+                .collect()
+        })
+        .collect();
+
+    for (i, block) in blocks.iter().enumerate() {
+        println!("probe {} sees {} beacons", i, block.len());
+    }
+
+    (0, 0)
+}
+
+#[test]
+fn test_example() {
+    assert_eq!(solve(EXAMPLE), (79, 0));
+}
+
+#[bench]
+fn bench_solve_current(b: &mut test::Bencher) {
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (0, 0));
+    });
+}
