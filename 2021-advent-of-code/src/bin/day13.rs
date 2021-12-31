b81678a2a80a652670c868b2acc6f93e552da29a
Ethan Ransom <derethanhausen@gmail.com>
2021-12-31 15:10:54 -0700

    aoc21: 13: solve!!!

--- 2021-advent-of-code/src/bin/day13.rs
+++ 2021-advent-of-code/src/bin/day13.rs
@@ -1,6 +1,7 @@
 #![feature(test)]
 
 use regex::Regex;
+use std::collections::HashSet;
 
 extern crate test;
 
@@ -15,15 +16,74 @@ fn main() {
 type Point = (usize, usize);
 
 fn solve(input: &str) -> (usize, usize) {
-    let (points, folds) = parse(input);
+    let (initial_points, folds) = parse(input);
 
-    dbg!(points);
+    let mut counts = folds
+        .iter()
+        .scan(Box::new(initial_points), |points, &fold| {
+            let new_points = points
+                .iter()
+                .map(|&point| fold_point(point, fold))
+                .collect();
 
-    dbg!(folds);
-    (0, 0)
+            print_points(&new_points);
+
+            println!("===========================");
+
+            *points = Box::new(new_points);
+            Some(points.clone())
+        });
+
+    let one = counts.next().unwrap().len();
+
+    let _ = counts.last().unwrap();
+
+    (one, 0)
 }
 
-fn parse(input: &str) -> (Vec<Point>, Vec<Point>) {
+fn fold_point((px, py): Point, fold: Point) -> Point {
+    match fold {
+        (fx, 0) => (fold_scalar(px, fx), py),
+        (0, fy) => (px, fold_scalar(py, fy)),
+        (_, _) => panic!(),
+    }
+}
+
+fn fold_scalar(point: usize, fold: usize) -> usize {
+    let protrusion = point as i64 - fold as i64;
+    if protrusion > 0 {
+        fold - protrusion as usize
+    } else {
+        point
+    }
+}
+
+fn print_points(points: &HashSet<Point>) {
+    let &x = points.iter().map(|(x, _)| x).max().unwrap();
+    let &y = points.iter().map(|(_, y)| y).max().unwrap();
+
+    for y in 0..=y {
+        for x in 0..=x {
+            if points.contains(&(x, y)) {
+                print!("#")
+            } else {
+                print!(" ")
+            }
+        }
+
+        println!();
+    }
+
+    dbg!(x, y);
+}
+
+#[test]
+fn test_fold_scalar() {
+    assert_eq!(fold_scalar(6, 8), 6);
+    assert_eq!(fold_scalar(8, 6), 4);
+}
+
+fn parse(input: &str) -> (HashSet<Point>, Vec<Point>) {
     let (points, folds) = input.split_once("\n\n").expect("expected two sections");
 
     let points = points
@@ -32,7 +92,7 @@ fn parse(input: &str) -> (Vec<Point>, Vec<Point>) {
             let (x, y) = line.split_once(',').expect("expected ','");
             (x.parse().unwrap(), y.parse().unwrap())
         })
-        .collect::<Vec<Point>>();
+        .collect();
 
     let regex = Regex::new(r"fold along (x|y)=(\d+)$").expect("couldn't compile regex");
 
