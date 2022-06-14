3c02202f4d010c5c7b2734faa2f11c1041b0fe14
Ethan Ransom <derethanhausen@gmail.com>
2022-06-14 00:45:38 -0600

    aoc21: 7: solve part 2
    this was originally written back in Dec, not sure exactly when

--- 2021-advent-of-code/src/bin/day07.rs
+++ 2021-advent-of-code/src/bin/day07.rs
@@ -10,12 +10,12 @@ fn main() {
     dbg!(solve(INPUT));
 }
 
-pub fn minimize_cost(crabs: &[u32], cost: fn(u32) -> u32) -> u32 {
+fn minimize_cost(crabs: &[u32], cost: fn(u32) -> u32) -> u32 {
     let mut min_fuel = None;
-    for i in 0..*crabs.iter().max().unwrap() {
+    for i in *crabs.iter().min().unwrap()..*crabs.iter().max().unwrap() {
         let mut fuel = 0;
         for &crab in crabs {
-            let dist = (crab - i) as usize;
+            let dist = (crab as i64 - i as i64).abs();
             fuel += cost(dist as u32);
         }
         // println!("for pos {} fuel of {}", i, fuel);
@@ -51,14 +51,61 @@ fn solve(input: &[u8]) -> (u32, u32) {
     )
 }
 
-#[test]
-fn test_example() {
-    assert_eq!(solve(EXAMPLE), (37, 168));
+#[bench]
+fn bench_solve_example(b: &mut test::Bencher) {
+    b.iter(|| solve(EXAMPLE));
+}
+
+#[bench]
+fn bench_solve_example_str(b: &mut test::Bencher) {
+    const EXAMPLE_STR: &str = include_str!("example07.txt");
+
+    pub fn minimize_cost(crabs: &[u32], cost: fn(u32) -> u32) -> u32 {
+        let mut min_fuel = None;
+        for i in *crabs.iter().min().unwrap()..*crabs.iter().max().unwrap() {
+            let mut fuel = 0;
+            for &crab in crabs {
+                let dist = (crab as i64 - i as i64).abs();
+                fuel += cost(dist as u32);
+            }
+            // println!("for pos {} fuel of {}", i, fuel);
+            match min_fuel {
+                None => min_fuel = Some(fuel),
+                Some(old_min) => {
+                    if fuel < old_min {
+                        min_fuel = Some(fuel)
+                    }
+                }
+            }
+        }
+        min_fuel.expect("NO SOLUTION")
+    }
+
+    fn solve(input: &str) -> (u32, u32) {
+        let crabs = input
+            .split(",")
+            .map(|n| n.parse().expect("not a number"))
+            .collect::<Vec<u32>>();
+
+        fn linear_cost(dist: u32) -> u32 {
+            dist
+        }
+        fn exp_cost(dist: u32) -> u32 {
+            (1..=dist).sum()
+        }
+
+        (
+            minimize_cost(&crabs, linear_cost),
+            minimize_cost(&crabs, exp_cost),
+        )
+    }
+
+    b.iter(|| solve(EXAMPLE_STR));
 }
 
 #[bench]
 fn bench_solve_current(b: &mut test::Bencher) {
     b.iter(|| {
-        assert_eq!(solve(INPUT), (342534, 0));
+        assert_eq!(solve(INPUT), (342534, 94004208));
     });
 }
