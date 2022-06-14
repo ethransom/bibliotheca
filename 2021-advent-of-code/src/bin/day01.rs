bd06f39267de6372c9a87207b9270eb17faaa01d
Ethan Ransom <derethanhausen@gmail.com>
2022-06-14 00:43:04 -0600

    aoc21: 1: switch to include_str!
    I didn't know about this at the time, all the benches around bytes are the same speed when just calling str.as_bytes()

--- 2021-advent-of-code/src/bin/day01.rs
+++ 2021-advent-of-code/src/bin/day01.rs
@@ -1,8 +1,8 @@
 #![feature(test)]
 #![feature(array_windows)]
 
-const EXAMPLE: &[u8] = include_bytes!("example01.txt");
-const INPUT: &[u8] = include_bytes!("input01.txt");
+const EXAMPLE: &str = include_str!("example01.txt");
+const INPUT: &str = include_str!("input01.txt");
 
 fn main() {
     println!("example:");
@@ -11,14 +11,18 @@ fn main() {
     println!("\t{:?}", solve(INPUT));
 }
 
-fn parse(dat: &[u8]) -> Vec<u64> {
-    dat.split(|b| *b == b'\n')
-        .map(|line| line.iter().fold(0_u64, |n, b| n * 10 + (b - b'0') as u64))
+fn parse(dat: &str) -> Vec<u64> {
+    dat.lines()
+        .map(|line| {
+            line.as_bytes()
+                .iter()
+                .fold(0_u64, |n, b| n * 10 + (b - b'0') as u64)
+        })
         .collect::<Vec<u64>>()
 }
 
 #[inline(always)]
-fn solve(input: &[u8]) -> (usize, usize) {
+fn solve(input: &str) -> (usize, usize) {
     let nums = parse(input);
     let simple_increases = nums.windows(2).filter(|w| w[1] > w[0]).count();
 
@@ -53,10 +57,10 @@ fn bench_parse_00(b: &mut test::Bencher) {
 }
 
 #[bench]
-fn bench_parse_01_unsafe_str(b: &mut test::Bencher) {
-    let str = unsafe { std::str::from_utf8_unchecked(INPUT) };
+fn bench_parse_01_lines_u64_parse(b: &mut test::Bencher) {
     b.iter(|| {
-        str.lines()
+        INPUT
+            .lines()
             .map(|line| line.parse::<u64>().unwrap())
             .collect::<Vec<u64>>()
     });
@@ -66,6 +70,7 @@ fn bench_parse_01_unsafe_str(b: &mut test::Bencher) {
 fn bench_parse_02_bytes(b: &mut test::Bencher) {
     b.iter(|| {
         INPUT
+            .as_bytes()
             .split(|b| *b == b'\n')
             .map(|line| {
                 std::str::from_utf8(line)
@@ -81,6 +86,7 @@ fn bench_parse_02_bytes(b: &mut test::Bencher) {
 fn bench_parse_03_bytes_flat_map(b: &mut test::Bencher) {
     b.iter(|| {
         INPUT
+            .as_bytes()
             .split(|b| *b == b'\n')
             .flat_map(|line| unsafe { std::str::from_utf8_unchecked(line) }.parse::<u32>())
             .collect::<Vec<u32>>()
@@ -91,6 +97,7 @@ fn bench_parse_03_bytes_flat_map(b: &mut test::Bencher) {
 fn bench_parse_04_bytes_unsafe(b: &mut test::Bencher) {
     b.iter(|| {
         INPUT
+            .as_bytes()
             .split(|b| *b == b'\n')
             .map(|line| {
                 unsafe { std::str::from_utf8_unchecked(line) }
@@ -105,6 +112,7 @@ fn bench_parse_04_bytes_unsafe(b: &mut test::Bencher) {
 fn bench_parse_05_bytes_u16(b: &mut test::Bencher) {
     b.iter(|| {
         INPUT
+            .as_bytes()
             .split(|b| *b == b'\n')
             .map(|line| {
                 unsafe { std::str::from_utf8_unchecked(line) }
@@ -119,6 +127,7 @@ fn bench_parse_05_bytes_u16(b: &mut test::Bencher) {
 fn bench_parse_06_bytes_custom(b: &mut test::Bencher) {
     b.iter(|| {
         INPUT
+            .as_bytes()
             .split(|b| *b == '\n' as u8)
             .map(|line| line.iter().fold(0_u64, |n, b| n * 10 + (b - b'0') as u64))
             .collect::<Vec<u64>>()
@@ -129,6 +138,7 @@ fn bench_parse_06_bytes_custom(b: &mut test::Bencher) {
 fn bench_parse_07_bytes_custom_u16(b: &mut test::Bencher) {
     b.iter(|| {
         INPUT
+            .as_bytes()
             .split(|b| *b == '\n' as u8)
             .map(|line| line.iter().fold(0_u16, |n, b| n * 10 + (b - b'0') as u16))
             .collect::<Vec<u16>>()
@@ -139,11 +149,14 @@ fn bench_parse_07_bytes_custom_u16(b: &mut test::Bencher) {
 fn bench_parse_08_bytes_size_hint(b: &mut test::Bencher) {
     b.iter(|| {
         let mut vec: Vec<u64> = Vec::with_capacity(2000);
-        INPUT.split(|b| *b == '\n' as u8).for_each(|line| {
-            let n = line.iter().fold(0_u64, |n, b| n * 10 + (b - b'0') as u64);
+        INPUT
+            .as_bytes()
+            .split(|b| *b == '\n' as u8)
+            .for_each(|line| {
+                let n = line.iter().fold(0_u64, |n, b| n * 10 + (b - b'0') as u64);
 
-            vec.push(n);
-        })
+                vec.push(n);
+            })
     });
 }
 
