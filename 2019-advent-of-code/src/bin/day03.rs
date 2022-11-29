a72ccd9f372d3ba6a2177559fd58782d650919f1
Ethan Ransom <derethanhausen@gmail.com>
2022-11-28 20:56:04 -0700

    aoc19: day03: solve part 2

--- 2019-advent-of-code/src/bin/day03.rs
+++ 2019-advent-of-code/src/bin/day03.rs
@@ -1,6 +1,6 @@
 #![feature(test)]
 
-use std::{collections::HashSet, hash::BuildHasherDefault};
+use std::{collections::HashMap, hash::BuildHasherDefault};
 
 use fxhash::FxHasher;
 
@@ -14,7 +14,7 @@ fn main() {
     dbg!(solve(INPUT));
 }
 
-type FastCoordinateSet = HashSet<(i64, i64), BuildHasherDefault<FxHasher>>;
+type FastCoordinateMap = HashMap<(i64, i64), u64, BuildHasherDefault<FxHasher>>;
 
 fn solve(input: &str) -> (u64, u64) {
     let wires: [Vec<&str>; 2] = input
@@ -25,9 +25,9 @@ fn solve(input: &str) -> (u64, u64) {
         .expect("expected two wires in input");
 
     let [path1, path2] = wires.map(|wire| {
-        let mut path: FastCoordinateSet = HashSet::default();
+        let mut path: FastCoordinateMap = HashMap::default();
 
-        wire.iter().fold((0, 0), |cursor, segment| {
+        wire.iter().fold(((0, 0), 0), |(cursor, dist), segment| {
             let (dir, amount) = (&segment[..1], &segment[1..]);
             let amount: u64 = amount.parse().expect("couldn't parse segment length");
 
@@ -39,24 +39,31 @@ fn solve(input: &str) -> (u64, u64) {
                 _ => panic!("unknown segment direction: '{}'", dir),
             };
 
-            (1..=amount).fold(cursor, |(x, y), _amount| {
-                let cursor = (x + unit.0, y + unit.1);
-                path.insert(cursor);
-                cursor
+            (1..=amount).fold((cursor, dist), |((x, y), dist), _amount| {
+                let (cursor, dist) = ((x + unit.0, y + unit.1), dist + 1);
+                path.insert(cursor, dist);
+                (cursor, dist)
             })
         });
 
         path
     });
 
-    let closest_intersection = path1
-        .iter()
-        .filter(|coord| path2.contains(coord))
-        .map(|p| manhattan_dist(*p))
+    let intersections = path1.keys().filter(|coord| path2.contains_key(coord));
+
+    let closest_intersection = intersections
+        .clone()
+        .cloned()
+        .map(manhattan_dist)
         .min()
         .expect("no wire intersections found");
 
-    (closest_intersection, 0)
+    let soonest_intersection = intersections
+        .map(|coord| path1.get(coord).unwrap() + path2.get(coord).unwrap())
+        .min()
+        .expect("no wire intersections found");
+
+    (closest_intersection, soonest_intersection)
 }
 
 fn manhattan_dist(coords: (i64, i64)) -> u64 {
@@ -65,11 +72,22 @@ fn manhattan_dist(coords: (i64, i64)) -> u64 {
 
 #[test]
 fn test_example() {
-    assert_eq!(solve(EXAMPLE), (6, 0));
+    assert_eq!(solve(EXAMPLE), (6, 30));
+}
+
+#[bench]
+fn bench_solve_part1_03_current(b: &mut test::Bencher) {
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (557, 56_410));
+    });
 }
 
 #[bench]
 fn bench_solve_part1_02_fxhash(b: &mut test::Bencher) {
+    use std::collections::HashSet;
+
+    type FastCoordinateSet = HashSet<(i64, i64), BuildHasherDefault<FxHasher>>;
+
     fn solve(input: &str) -> (u64, u64) {
         let wires: [Vec<&str>; 2] = input
             .lines()
@@ -120,6 +138,8 @@ fn bench_solve_part1_02_fxhash(b: &mut test::Bencher) {
 
 #[bench]
 fn bench_solve_part1_01_original(b: &mut test::Bencher) {
+    use std::collections::HashSet;
+
     fn solve(input: &str) -> (u64, u64) {
         let wires: [Vec<&str>; 2] = input
             .lines()
