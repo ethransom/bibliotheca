74d4c501670460db8bc21744bebb275b8946471b
Ethan Ransom <derethanhausen@gmail.com>
2022-11-28 20:27:36 -0700

    aoc19: day03: inline part1 solution into benchmark

--- 2019-advent-of-code/src/bin/day03.rs
+++ 2019-advent-of-code/src/bin/day03.rs
@@ -69,14 +69,57 @@ fn test_example() {
 }
 
 #[bench]
-fn bench_solve_02_current(b: &mut test::Bencher) {
+fn bench_solve_part1_02_fxhash(b: &mut test::Bencher) {
+    fn solve(input: &str) -> (u64, u64) {
+        let wires: [Vec<&str>; 2] = input
+            .lines()
+            .map(|line| line.split(',').collect())
+            .collect::<Vec<Vec<&str>>>()
+            .try_into()
+            .expect("expected two wires in input");
+
+        let [path1, path2] = wires.map(|wire| {
+            let mut path: FastCoordinateSet = HashSet::default();
+
+            wire.iter().fold((0, 0), |cursor, segment| {
+                let (dir, amount) = (&segment[..1], &segment[1..]);
+                let amount: u64 = amount.parse().expect("couldn't parse segment length");
+
+                let unit = match dir {
+                    "U" => (0, 1),
+                    "R" => (1, 0),
+                    "D" => (0, -1),
+                    "L" => (-1, 0),
+                    _ => panic!("unknown segment direction: '{}'", dir),
+                };
+
+                (1..=amount).fold(cursor, |(x, y), _amount| {
+                    let cursor = (x + unit.0, y + unit.1);
+                    path.insert(cursor);
+                    cursor
+                })
+            });
+
+            path
+        });
+
+        let closest_intersection = path1
+            .iter()
+            .filter(|coord| path2.contains(coord))
+            .map(|p| manhattan_dist(*p))
+            .min()
+            .expect("no wire intersections found");
+
+        (closest_intersection, 0)
+    }
+
     b.iter(|| {
         assert_eq!(solve(INPUT), (557, 0));
     });
 }
 
 #[bench]
-fn bench_solve_01_original(b: &mut test::Bencher) {
+fn bench_solve_part1_01_original(b: &mut test::Bencher) {
     fn solve(input: &str) -> (u64, u64) {
         let wires: [Vec<&str>; 2] = input
             .lines()
