73a6d485da07b0445c4642ab2c8adce629c82714
Ethan Ransom <derethanhausen@gmail.com>
2022-12-03 11:51:42 -0700

    aoc22: 1: noalloc to go 2.3x faster than original

--- 2022-advent-of-code/src/bin/day01.rs
+++ 2022-advent-of-code/src/bin/day01.rs
@@ -1,4 +1,6 @@
 #![feature(test)]
+#![feature(is_some_and)]
+#![feature(let_chains)]
 
 use std::num::ParseIntError;
 
@@ -66,6 +68,52 @@ fn bench_solve_01_onealloc(b: &mut test::Bencher) {
 
         (max, top_three)
     }
+
+    assert_eq!(solve(EXAMPLE), (24_000, 45_000));
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (71_023, 206_289));
+    });
+}
+
+#[bench]
+fn bench_solve_02_noalloc(b: &mut test::Bencher) {
+    fn solve(input: &str) -> (u32, u32) {
+        let top_three = input
+            .split("\n\n")
+            .try_fold([Option::None::<u32>; 3], |mut top_three, line| {
+                line.lines()
+                    .try_fold(0, |sum, item| {
+                        str::parse::<u32>(item).and_then(|i| Ok(sum + i))
+                    })
+                    .and_then(|calories| {
+                        // refactor: extract this into a StaticNHeap or similar
+                        for i in top_three.iter_mut() {
+                            if i.is_none() {
+                                *i = Some(calories);
+                                return Ok(top_three);
+                            }
+                        }
+
+                        let min = top_three.iter_mut().min().unwrap();
+                        if min.unwrap() < calories {
+                            *min = Some(calories);
+                        }
+
+                        Ok(top_three)
+                    })
+            })
+            .expect("couldn't parse calories")
+            .map(|i| i.expect("not enough to get top three"));
+
+        let &max = top_three.iter().max().unwrap();
+
+        let top_three = top_three.iter().sum();
+
+        (max, top_three)
+    }
+
+    assert_eq!(solve(EXAMPLE), (24_000, 45_000));
 
     b.iter(|| {
         assert_eq!(solve(INPUT), (71_023, 206_289));
