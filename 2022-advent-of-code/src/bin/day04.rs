bf92d083d7a1ebbdf71e16d71c9a1860ca5deab8
Ethan Ransom <derethanhausen@gmail.com>
2022-12-04 01:15:44 -0700

    aoc22: day04: part 1

---
+++ 2022-advent-of-code/src/bin/day04.rs
@@ -0,0 +1,57 @@
+#![feature(test)]
+
+extern crate test;
+
+const EXAMPLE: &str = include_str!("example04.txt");
+const INPUT: &str = include_str!("input04.txt");
+
+fn main() {
+    dbg!(solve(EXAMPLE));
+    dbg!(solve(INPUT));
+}
+
+fn solve(input: &str) -> (usize, usize) {
+    let assignments = parse(input);
+
+    let overlaps = assignments
+        .iter()
+        .filter(|(a, b)| is_fully_contained(a, b) || is_fully_contained(b, a))
+        .count();
+
+    (overlaps, 0)
+}
+
+fn is_fully_contained(range1: &(u32, u32), range2: &(u32, u32)) -> bool {
+    range1.0 <= range2.0 && range1.1 >= range2.1
+}
+
+fn parse(input: &str) -> Vec<((u32, u32), (u32, u32))> {
+    input
+        .lines()
+        .map(|line| {
+            let (left, right) = line.split_once(',').expect("expected two assignments");
+
+            let ((a, b), (c, d)) = (
+                left.split_once('-').unwrap(),
+                right.split_once('-').unwrap(),
+            );
+
+            (
+                (a.parse().unwrap(), b.parse().unwrap()),
+                (c.parse().unwrap(), d.parse().unwrap()),
+            )
+        })
+        .collect()
+}
+
+#[test]
+fn test_example() {
+    assert_eq!(solve(EXAMPLE), (2, 0));
+}
+
+#[bench]
+fn bench_solve_current(b: &mut test::Bencher) {
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (540, 0));
+    });
+}
