5602dd992e88381e814683143ce274193c3963f3
Ethan Ransom <derethanhausen@gmail.com>
2022-12-04 14:27:19 -0700

    lumen2: parse more of the air quality station data
    TODO: why tf can't floats work??

--- lumen2/src/station_feed.rs
+++ lumen2/src/station_feed.rs
@@ -1,5 +1,11 @@
 use serde::Deserialize;
-use serde_xml_rs::{from_str, to_string};
+use serde_xml_rs::from_str;
+
+impl AirQualityData {
+    pub fn parse(str: &str) -> Result<AirQualityData, serde_xml_rs::Error> {
+        from_str(str)
+    }
+}
 
 #[derive(Debug, Deserialize)]
 struct AirQualityData {
@@ -10,36 +16,39 @@ struct AirQualityData {
 #[derive(Debug, Deserialize)]
 struct Site {
     name: String,
-    // data: Option<Vec<Data>>,
+    data: Vec<Data>,
 }
 
 #[derive(Debug, Deserialize)]
 struct Data {
-    date: String, // TODO: parse?
-    ozone: f32,
-    ozone_8hr_avg: f32,
-    pm25: f32,
-    pm25_24hr_avg: f32,
-    nox: f32,
-    no2: f32,
-    temperature: f32,
+    date: String, // TODO: parse date?
+    // ozone: f64, // TODO: why can't these parse as floats?
+    // ozone_8hr_avg: f32,
+    // pm25: f32,
+    // pm25_24hr_avg: f32,
+    // nox: f32,
+    // no2: f32,
+    // temperature: f32,
     relative_humidity: i8,
-    wind_speed: f32,
-    wind_direction: i32,
-    co: f32,
-    solar_radiation: f32,
-    so2: f32,
+    // wind_speed: f32,
+    // wind_direction: i32,
+    // co: f32,
+    // solar_radiation: f32,
+    // so2: f32,
 }
 
 #[test]
 fn test_station_feed_deserialization() {
     let fixture = include_str!("roseParkStationFeedResponse.xml");
 
-    let data: AirQualityData = match from_str(fixture) {
+    let data = match AirQualityData::parse(fixture) {
         Err(e) => panic!("{e:?}"),
         Ok(d) => d,
     };
 
     assert_eq!(data.state, "Utah");
     assert_eq!(data.site.name, "Rose Park");
+
+    assert_eq!(data.site.data.len(), 239);
+    assert_eq!(data.site.data.first().unwrap().date, "12/04/2022 12:00:00");
 }
