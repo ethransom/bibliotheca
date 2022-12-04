bd6b68c5aaca2925a7858b225fe9fa47f789696e
Ethan Ransom <derethanhausen@gmail.com>
2022-12-04 00:10:17 -0700

    aoc22: day03: part one with moar set math

---
+++ 2022-advent-of-code/src/bin/day03.rs
@@ -0,0 +1,53 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::collections::HashSet;
+
+const EXAMPLE: &str = include_str!("example03.txt");
+const INPUT: &str = include_str!("input03.txt");
+
+fn main() {
+    dbg!(solve(EXAMPLE));
+    dbg!(solve(INPUT));
+}
+
+fn solve(input: &str) -> (usize, usize) {
+    let lines = parse(input);
+
+    let priorities_of_splits = lines
+        .map(|line| {
+            let [items1, items2]: [HashSet<char>; 2] = [
+                line[..line.len() / 2].chars().collect::<HashSet<char>>(),
+                line[line.len() / 2..].chars().collect::<HashSet<char>>(),
+            ];
+
+            HashSet::intersection(&items1, &items2)
+                .map(|&item| {
+                    1 + ('a'..='z')
+                        .chain('A'..='Z')
+                        .position(|c| c == item)
+                        .unwrap_or_else(|| panic!("unknown item type {item}"))
+                })
+                .sum::<usize>()
+        })
+        .sum();
+
+    (priorities_of_splits, 0)
+}
+
+fn parse(input: &str) -> impl Iterator<Item = &str> {
+    input.lines()
+}
+
+#[test]
+fn test_example() {
+    assert_eq!(solve(EXAMPLE), (157, 0));
+}
+
+#[bench]
+fn bench_solve_current(b: &mut test::Bencher) {
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (7_727, 0));
+    });
+}
