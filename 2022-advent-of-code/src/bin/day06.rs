17d9df44f073a9ac621420f0bbbc02449dda338d
Ethan Ransom <derethanhausen@gmail.com>
2022-12-05 22:53:02 -0700

    aoc22: day06: gotta go fast

--- 2022-advent-of-code/src/bin/day06.rs
+++ 2022-advent-of-code/src/bin/day06.rs
@@ -51,7 +51,41 @@ fn first_distinct_window(input: &str, size: usize) -> Option<usize> {
 }
 
 #[bench]
-fn bench_solve_current(b: &mut test::Bencher) {
+fn bench_solve_00_original(b: &mut test::Bencher) {
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (1953, 2301));
+    });
+}
+
+#[bench]
+fn bench_solve_01_better(b: &mut test::Bencher) {
+    fn solve(input: &str) -> (usize, usize) {
+        (
+            first_distinct_window(input, 4).unwrap(),
+            first_distinct_window(input, 14).unwrap(),
+        )
+    }
+
+    fn first_distinct_window(input: &str, size: usize) -> Option<usize> {
+        input
+            .chars()
+            .collect::<Vec<char>>()
+            .windows(size)
+            .position(|window| {
+                window
+                    .iter()
+                    .enumerate()
+                    .all(|(i, _)| !window[i + 1..].contains(&window[i]))
+            })
+            .map(|pos| pos + size)
+    }
+
+    assert_eq!(solve("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), (7, 19));
+    assert_eq!(solve("bvwbjplbgvbhsrlpgdmjqwftvncz"), (5, 23));
+    assert_eq!(solve("nppdvjthqldpwncqszvftbrmjlhg"), (6, 23));
+    assert_eq!(solve("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), (10, 29));
+    assert_eq!(solve("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), (11, 26));
+
     b.iter(|| {
         assert_eq!(solve(INPUT), (1953, 2301));
     });
