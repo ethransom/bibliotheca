c8b1239e169c290b67c975cdba15cce30039375a
Ethan Ransom <derethanhausen@gmail.com>
2022-12-08 21:54:05 -0700

    aoc22: day08: part 2 with equally naive algo

--- 2022-advent-of-code/src/bin/day08.rs
+++ 2022-advent-of-code/src/bin/day08.rs
@@ -13,19 +13,17 @@ fn main() {
 fn solve(input: &str) -> (usize, usize) {
     let grid = parse(input).expect("couldn't parse input");
 
-    dbg!(&grid);
-
     let visible = grid
         .iter()
         .enumerate()
         .map(|(r, row)| {
             row.iter()
                 .enumerate()
-                .filter(|(c, &cell)| {
+                .filter(|&(c, &cell)| {
                     let directions = [(-1, 0), (0, -1), (0, 1), (1, 0)];
 
                     directions.iter().any(|direction| {
-                        let mut x = *c as i32;
+                        let mut x = c as i32;
                         let mut y = r as i32;
 
                         while x > 0
@@ -48,7 +46,49 @@ fn solve(input: &str) -> (usize, usize) {
         })
         .sum();
 
-    (visible, 0)
+    let best_view: usize = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(r, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(c, &cell)| {
+                    let directions = [(-1, 0), (0, -1), (0, 1), (1, 0)];
+
+                    directions
+                        .iter()
+                        .map(|direction| -> usize {
+                            let mut x = c as i32;
+                            let mut y = r as i32;
+
+                            let mut visible = 0;
+
+                            while x > 0
+                                && y > 0
+                                && x < row.len() as i32 - 1
+                                && y < grid.len() as i32 - 1
+                            {
+                                x += direction.0;
+                                y += direction.1;
+
+                                visible += 1;
+
+                                if grid[y as usize][x as usize] >= cell {
+                                    break;
+                                    // view PAST, but not including, this cell is blocked
+                                }
+                            }
+
+                            visible
+                        })
+                        .product()
+                })
+                .max()
+        })
+        .max()
+        .expect("no trees with view score");
+
+    (visible, best_view)
 }
 
 fn parse(input: &str) -> Option<Vec<Vec<u8>>> {
@@ -64,12 +104,12 @@ fn parse(input: &str) -> Option<Vec<Vec<u8>>> {
 
 #[test]
 fn test_example() {
-    assert_eq!(solve(EXAMPLE), (21, 0));
+    assert_eq!(solve(EXAMPLE), (21, 8));
 }
 
 #[bench]
 fn bench_solve_current(b: &mut test::Bencher) {
     b.iter(|| {
-        assert_eq!(solve(INPUT), (1849, 0));
+        assert_eq!(solve(INPUT), (1_849, 201_600));
     });
 }
