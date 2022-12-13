6c6b27a6b5db613848b2c0b50f8a06c97ad93508
Ethan Ransom <derethanhausen@gmail.com>
2022-12-12 20:39:46 -0700

    aoc22: 12: 20x faster by going end -> start

--- 2022-advent-of-code/src/bin/day12.rs
+++ 2022-advent-of-code/src/bin/day12.rs
@@ -15,7 +15,11 @@ fn main() {
 fn solve(input: &str) -> (usize, usize) {
     let (heightmap, start, end) = parse(input);
 
-    let best_path = find_path(&heightmap, start, end).expect("no path found");
+    let distances = find_path(&heightmap, end, start);
+
+    print_grid(&distances);
+
+    let best_path = distances[start.1][start.0].expect("no path found");
 
     let best_path_any_a = heightmap
         .iter()
@@ -24,7 +28,7 @@ fn solve(input: &str) -> (usize, usize) {
             row.iter()
                 .enumerate()
                 .filter(|(_x, &h)| h == 0)
-                .flat_map(|(x, _h)| find_path(&heightmap, (x, y), end))
+                .flat_map(|(x, _h)| distances[y][x])
                 .min()
         })
         .min()
@@ -70,7 +74,7 @@ fn find_path(
     heightmap: &Vec<Vec<u8>>,
     start: (usize, usize),
     end: (usize, usize),
-) -> Option<usize> {
+) -> Vec<Vec<Option<usize>>> {
     let mut distances: Vec<Vec<Option<usize>>> =
         heightmap.iter().map(|row| vec![None; row.len()]).collect();
 
@@ -113,8 +117,8 @@ fn find_path(
             }
 
             let (height, neighbor_height) = (heightmap[node.1][node.0], heightmap[y][x]);
-            if neighbor_height > height + 1 {
-                // unreachable, can only climb 1 unit at a time
+            if neighbor_height + 1 < height {
+                // unreachable, can descend one and ascend as many as we want
                 continue;
             }
 
@@ -125,7 +129,7 @@ fn find_path(
 
     print_grid(&distances);
 
-    distances[end.1][end.0]
+    distances
 }
 
 fn print_grid(distances: &Vec<Vec<Option<usize>>>) {
@@ -198,8 +202,98 @@ fn test_example() {
     assert_eq!(solve(EXAMPLE), (31, 29));
 }
 
+/// Original solution, solves `start -> end` and `any a -> end`.
+/// Results in many many calls to find_path, very slow.
 #[bench]
-fn bench_solve_current(b: &mut test::Bencher) {
+fn bench_solve_00_original(b: &mut test::Bencher) {
+    fn solve(input: &str) -> (usize, usize) {
+        let (heightmap, start, end) = parse(input);
+
+        let best_path = find_path(&heightmap, start, end).expect("no path found");
+
+        let best_path_any_a = heightmap
+            .iter()
+            .enumerate()
+            .flat_map(|(y, row)| {
+                row.iter()
+                    .enumerate()
+                    .filter(|(_x, &h)| h == 0)
+                    .flat_map(|(x, _h)| find_path(&heightmap, (x, y), end))
+                    .min()
+            })
+            .min()
+            .expect("no path found");
+
+        (best_path, best_path_any_a)
+    }
+
+    fn find_path(
+        heightmap: &Vec<Vec<u8>>,
+        start: (usize, usize),
+        end: (usize, usize),
+    ) -> Option<usize> {
+        let mut distances: Vec<Vec<Option<usize>>> =
+            heightmap.iter().map(|row| vec![None; row.len()]).collect();
+
+        distances[start.1][start.0] = Some(0);
+
+        let mut visited: Vec<Vec<_>> = heightmap.iter().map(|row| vec![false; row.len()]).collect();
+
+        let mut queue = VecDeque::<(usize, usize)>::new();
+        queue.push_back(start);
+
+        while let Some(node) = queue.pop_front() {
+            let dist = distances[node.1][node.0].unwrap();
+
+            if node == end {
+                break; // is this just an optimization? Or does it matter for correctness?
+            }
+
+            visited[node.1][node.0] = true;
+
+            for (dx, dy) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                let x = node.0 as i32 + dx;
+                let y = node.1 as i32 + dy;
+
+                if x < 0 || y < 0 || x >= heightmap[0].len() as i32 || y >= heightmap.len() as i32 {
+                    continue;
+                }
+
+                let x = x as usize;
+                let y = y as usize;
+
+                // TODO: do we need this?????
+                if visited[y][x] {
+                    // already visited
+                    continue;
+                }
+
+                if distances[y][x].is_some() {
+                    // already queued
+                    continue;
+                }
+
+                let (height, neighbor_height) = (heightmap[node.1][node.0], heightmap[y][x]);
+                if neighbor_height > height + 1 {
+                    // unreachable, can only climb 1 unit at a time
+                    continue;
+                }
+
+                distances[y][x] = Some(dist + 1);
+                queue.push_back((x, y));
+            }
+        }
+
+        distances[end.1][end.0]
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (472, 465));
+    });
+}
+
+#[bench]
+fn bench_solve_01_current(b: &mut test::Bencher) {
     b.iter(|| {
         assert_eq!(solve(INPUT), (472, 465));
     });
