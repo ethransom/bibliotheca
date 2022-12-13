720f2f6f51d504afd4c4ed6880097e8c8a00fc8c
Ethan Ransom <derethanhausen@gmail.com>
2022-12-12 18:57:06 -0700

    aoc22: 12: BFS and pretty printing

--- 2022-advent-of-code/src/bin/day12.rs
+++ 2022-advent-of-code/src/bin/day12.rs
@@ -1,6 +1,6 @@
 #![feature(test)]
 
-use itertools::Itertools;
+use std::collections::VecDeque;
 
 extern crate test;
 
@@ -15,14 +15,14 @@ fn main() {
 fn solve(input: &str) -> (usize, usize) {
     let (heightmap, start, end) = parse(input);
 
-    dbg!(
-        heightmap
-            .iter()
-            .map(|row| row.iter().join(", "))
-            .collect::<Vec<_>>(),
-        start,
-        end
-    );
+    // dbg!(
+    //     heightmap
+    //         .iter()
+    //         .map(|row| row.iter().join(", "))
+    //         .collect::<Vec<_>>(),
+    //     start,
+    //     end
+    // );
 
     let best_path = find_path(&heightmap, start, end);
 
@@ -70,12 +70,20 @@ fn find_path(heightmap: &Vec<Vec<u8>>, start: (usize, usize), end: (usize, usize
 
     let mut visited: Vec<Vec<_>> = heightmap.iter().map(|row| vec![false; row.len()]).collect();
 
-    let mut queue = vec![(start, 0)];
+    let mut queue = VecDeque::<(usize, usize)>::new();
+    queue.push_back(start);
 
-    while let Some((i, _)) = queue.iter().enumerate().min_by_key(|(_i, (_n, d))| d) {
-        let (node, dist) = queue.remove(i);
+    while let Some(node) = queue.pop_front() {
+        let dist = distances[node.1][node.0].unwrap();
 
-        println!("VISIT: {:?} reachable with cost ({})", node, dist);
+        // println!("VISIT: {:?} distance {}", node, dist);
+
+        if node == end {
+            // println!("FOUND END: {:?} distance {}", node, dist);
+            // return dist;
+            // return Some(dist);
+            break; // is this just an optimization? Or does it matter for correctness?
+        }
 
         visited[node.1][node.0] = true;
 
@@ -90,46 +98,107 @@ fn find_path(heightmap: &Vec<Vec<u8>>, start: (usize, usize), end: (usize, usize
             let x = x as usize;
             let y = y as usize;
 
-            println!("examining neighbor {:?}", (x, y));
+            // println!("examining neighbor {:?}", (x, y));
 
+            // TODO: do we need this?????
             if visited[y][x] {
-                println!("already visited");
+                // println!("already visited");
                 // already visited
                 continue;
             }
 
-            if u8::abs_diff(heightmap[y][x], heightmap[node.1][node.0]) > 1 {
-                println!(
-                    "was unreachable {} vs {}",
-                    heightmap[y][x], heightmap[node.1][node.0]
-                );
+            if distances[y][x].is_some() {
+                // println!("already queued");
+                // already queued
+                continue;
+            }
+
+            if heightmap[y][x]
+                .checked_sub(heightmap[node.1][node.0])
+                .map_or(true, |diff| diff > 1)
+            {
+                // println!(
+                //     "was unreachable {} vs {}",
+                //     heightmap[y][x], heightmap[node.1][node.0]
+                // );
                 // unreachable, can only climb 1 unit at a time
                 continue;
             }
 
-            if distances[y][x].is_none() || distances[y][x].unwrap() > dist + 1 {
-                println!(
-                    "better path to {:?} found cost {} vs {:?}",
-                    (x, y),
-                    dist + 1,
-                    distances[y][x]
-                );
-                distances[y][x] = Some(dist + 1);
-                queue.push(((x, y), dist + 1));
-            }
+            // println!("distance to {:?} is {}", (x, y), dist + 1,);
+            distances[y][x] = Some(dist + 1);
+            queue.push_back((x, y));
         }
     }
 
-    dbg!(distances
-        .iter()
-        .map(|row| {
-            row.iter()
-                .map(|cell| cell.map_or("-".to_owned(), |i| i.to_string()))
-                .join(", ")
-        })
-        .collect::<Vec<_>>());
+    print_grid(&distances);
 
     distances[end.1][end.0].expect("no path found")
+}
+
+fn print_grid(distances: &Vec<Vec<Option<usize>>>) {
+    let width = distances
+        .iter()
+        .flat_map(|row| row.iter().flatten().max())
+        .max()
+        .unwrap()
+        .to_string()
+        .len();
+
+    if distances.iter().map(|row| row.len()).max().unwrap() * (width + 1) > 100 {
+        // portrait
+        print!("{:width$} ", "", width = width + 1);
+        for row in 0..distances.len() {
+            print!("{:width$}", row, width = width + 1);
+        }
+        println!();
+
+        print!("{:width$}╔", "", width = width + 1);
+        for _ in 0..distances.len() {
+            print!("{:═^width$}", "═", width = width + 1);
+        }
+        println!();
+
+        for col in 0..distances[0].len() {
+            print!("{:width$}║", col.to_string(), width = width + 1);
+
+            for row in distances {
+                let cell = row[col];
+                print!(
+                    "{:>width$}",
+                    cell.map_or("-".to_owned(), |i| i.to_string()),
+                    width = width + 1
+                );
+            }
+            println!();
+        }
+    } else {
+        // landscape
+        print!("{:width$} ", "", width = width + 1);
+        for row in 0..distances[0].len() {
+            print!("{:width$}", row, width = width + 1);
+        }
+        println!();
+
+        print!("{:pad_width$}╔", "", pad_width = width + 1);
+        for _ in 0..distances[0].len() {
+            print!("{:═^width$}", "═", width = width + 1);
+        }
+        println!();
+
+        for (r, row) in distances.iter().enumerate() {
+            print!("{:width$}║", r.to_string(), width = width + 1);
+
+            for cell in row {
+                print!(
+                    "{:>width$}",
+                    cell.map_or("-".to_owned(), |i| i.to_string()),
+                    width = width + 1
+                );
+            }
+            println!();
+        }
+    }
 }
 
 #[test]
