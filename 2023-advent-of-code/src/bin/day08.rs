ed50f9086ce0120b33616994d9b84a252e76c8bd
Ethan Ransom <derethanhausen@gmail.com>
2023-12-08 17:19:51 -0700

    aoc23: day08: naive (bad) sol'n, doesn't work

--- 2023-advent-of-code/src/bin/day08.rs
+++ 2023-advent-of-code/src/bin/day08.rs
@@ -5,16 +5,46 @@
 use std::collections::HashMap;
 
 const EXAMPLE: &str = include_str!("example08.txt");
+const EXAMPLE2: &str = include_str!("example08_2.txt");
 const INPUT: &str = include_str!("input08.txt");
 
 fn main() {
-    dbg!(solve(EXAMPLE));
-    dbg!(solve(INPUT));
+    let (instructions, network) = parse(EXAMPLE);
+    let steps = simple_follow(&network, &instructions);
+    dbg!(steps);
+
+    let (instructions, network) = parse(EXAMPLE2);
+    let all_steps = ghost_follow(&network, &instructions);
+    dbg!(all_steps);
+
+    let (instructions, network) = parse(INPUT);
+    let steps = simple_follow(&network, &instructions);
+    let all_steps = ghost_follow(&network, &instructions);
+    dbg!((steps, all_steps));
 }
 
-fn solve(input: &str) -> (usize, usize) {
-    let (instructions, network) = parse(input);
+fn ghost_follow(network: &HashMap<&str, (&str, &str)>, instructions: &[char]) -> usize {
+    let mut locations = network
+        .keys()
+        .cloned() // god, I love rust
+        .filter(|&k| k.ends_with('A'))
+        .collect::<Vec<_>>();
+    let mut steps = 0;
+    let mut instructions = instructions.iter().cycle();
+    while locations.iter().any(|&l| !l.ends_with('Z')) {
+        let instruction = *instructions.next().unwrap();
+        for location in locations.iter_mut() {
+            let (left, right) = network.get(location).unwrap();
 
+            *location = if instruction == 'L' { left } else { right };
+        }
+
+        steps += 1;
+    }
+    steps
+}
+
+fn simple_follow(network: &HashMap<&str, (&str, &str)>, instructions: &Vec<char>) -> usize {
     let mut location = "AAA";
     let mut steps = 0;
     let mut instructions = instructions.iter().cycle();
@@ -30,7 +60,7 @@ fn solve(input: &str) -> (usize, usize) {
         steps += 1;
     }
 
-    (steps, 0)
+    steps
 }
 
 fn parse(input: &str) -> (Vec<char>, HashMap<&str, (&str, &str)>) {
@@ -53,12 +83,29 @@ fn parse(input: &str) -> (Vec<char>, HashMap<&str, (&str, &str)>) {
 
 #[test]
 fn test_example() {
-    assert_eq!(solve(EXAMPLE), (6, 0));
+    let (instructions, network) = parse(EXAMPLE);
+
+    let steps = simple_follow(&network, &instructions);
+
+    assert_eq!(steps, 6);
+}
+
+#[test]
+fn test_example2() {
+    let (instructions, network) = parse(EXAMPLE2);
+
+    let all_steps = ghost_follow(&network, &instructions);
+    assert_eq!(all_steps, 6);
 }
 
 #[test]
 fn test_input() {
-    assert_eq!(solve(INPUT), (18827, 0));
+    let (instructions, network) = parse(INPUT);
+
+    let steps = simple_follow(&network, &instructions);
+    let all_steps = ghost_follow(&network, &instructions);
+
+    assert_eq!((steps, all_steps), (18827, 0));
 }
 
 // #[bench]
