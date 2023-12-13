73703ab3a282495ae06946fd9aecafd59c8908a6
Ethan Ransom <derethanhausen@gmail.com>
2023-12-12 23:11:02 -0700

    aoc23: day11: part 1 🌌

---
+++ 2023-advent-of-code/src/bin/day11.rs
@@ -0,0 +1,121 @@
+// #![feature(test)]
+
+// extern crate test;
+
+use std::collections::HashSet;
+
+const EXAMPLE: &str = include_str!("example11.txt");
+const INPUT: &str = include_str!("input11.txt");
+
+fn main() {
+    dbg!(solve(EXAMPLE));
+    dbg!(solve(INPUT));
+}
+
+fn solve(input: &str) -> (usize, usize) {
+    let image = parse(input);
+
+    print(&image);
+
+    let ((x_min, x_max), (y_min, y_max)) = min_max(&image);
+
+    let y_expansions = (y_min..=y_max)
+        .filter(|y| (x_min..=x_max).all(|x| !image.contains(&(x, *y))))
+        .collect::<Vec<_>>();
+    let x_expansions = (x_min..=x_max)
+        .filter(|x| (y_min..=y_max).all(|y| !image.contains(&(*x, y))))
+        .collect::<Vec<_>>();
+
+    println!("y_expansions: {:?}", y_expansions);
+    println!("x_expansions: {:?}", x_expansions);
+
+    let image = image
+        .iter()
+        .copied()
+        .map(|(x, y)| {
+            (
+                x + x_expansions.iter().filter(|&&x2| x >= x2).count(),
+                y + y_expansions.iter().filter(|&&y2| y >= y2).count(),
+            )
+        })
+        .collect::<HashSet<_>>();
+
+    print(&image);
+
+    let image = image.into_iter().collect::<Vec<_>>();
+
+    let mut pairs = vec![];
+    let mut image = &image[..];
+    while image.len() > 1 {
+        let a = &image[0];
+        for b in &image[1..] {
+            println!("pair: {a:?} {b:?}");
+            pairs.push((a, b));
+        }
+        image = &image[1..];
+    }
+
+    let mut sum = 0;
+    for (a, b) in pairs {
+        let manhattan_dist =
+            (a.0 as i64 - b.0 as i64).unsigned_abs() + (a.1 as i64 - b.1 as i64).unsigned_abs();
+
+        sum += manhattan_dist as usize;
+    }
+
+    (sum, 0)
+}
+
+fn print(image: &HashSet<(usize, usize)>) {
+    let ((x_min, x_max), (y_min, y_max)) = min_max(image);
+
+    for y in y_min..=y_max {
+        for x in x_min..=x_max {
+            print!("{}", if image.contains(&(x, y)) { '#' } else { '.' });
+        }
+        println!();
+    }
+}
+
+fn min_max(image: &HashSet<(usize, usize)>) -> ((usize, usize), (usize, usize)) {
+    image.iter().fold(
+        ((0, 0), (0, 0)),
+        |((x_min, x_max), (y_min, y_max)), (x, y)| {
+            (
+                (x_min.min(*x), x_max.max(*x)),
+                (y_min.min(*y), y_max.max(*y)),
+            )
+        },
+    )
+}
+
+fn parse(input: &str) -> HashSet<(usize, usize)> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().flat_map(move |(x, c)| match c {
+                '.' => None,
+                '#' => Some((x, y)),
+                _ => panic!("invalid char: {}", c),
+            })
+        })
+        .collect()
+}
+
+#[test]
+fn test_example() {
+    assert_eq!(solve(EXAMPLE), (374, 0));
+}
+
+#[test]
+fn test_input() {
+    assert_eq!(solve(INPUT), (9563821, 0));
+}
+
+// #[bench]
+// fn bench_solve_current(b: &mut test::Bencher) {
+//     b.iter(|| {
+//         assert_eq!(solve(INPUT), (0, 0));
+//     });
+// }
