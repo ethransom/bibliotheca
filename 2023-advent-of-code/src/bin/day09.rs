0ad5e4bb7cec043b0e44071aaf5b0710684e591e
Ethan Ransom <derethanhausen@gmail.com>
2023-12-12 22:18:52 -0700

    aoc23: day09: some benching shenanigans

--- 2023-advent-of-code/src/bin/day09.rs
+++ 2023-advent-of-code/src/bin/day09.rs
@@ -97,3 +97,225 @@ fn bench_solve_current(b: &mut test::Bencher) {
         test_input();
     });
 }
+
+#[bench]
+fn bench_solve_without_original_clones(b: &mut test::Bencher) {
+    fn parse(input: &str) -> Option<Vec<Vec<i32>>> {
+        input
+            .lines()
+            .map(|line| line.split_whitespace().map(str::parse::<i32>).collect())
+            .collect::<Result<_, _>>()
+            .ok()
+    }
+
+    #[allow(dead_code)]
+    fn solve(input: &str) -> (i32, i32) {
+        let histories = parse(input).expect("couldn't parse input");
+
+        histories.into_iter().fold((0, 0), |(next, prev), history| {
+            let mut derivatives = vec![history];
+
+            loop {
+                let derivative = derivatives
+                    .last()
+                    .unwrap()
+                    .iter()
+                    .as_slice()
+                    .windows(2)
+                    .map(|pair| pair[1] - pair[0])
+                    .collect::<Vec<_>>();
+
+                let brk = derivative
+                    .iter()
+                    .as_slice()
+                    .windows(2)
+                    .all(|w| w[0] == w[1]);
+
+                derivatives.push(derivative);
+
+                if brk {
+                    break;
+                }
+            }
+
+            #[cfg(debug_assertions)]
+            {
+                for d in &derivatives {
+                    println!("{:?}", d);
+                }
+                println!(
+                    "solved {history:?} after {steps} derivations",
+                    history = derivatives.first().unwrap(),
+                    steps = derivatives.len()
+                );
+            }
+
+            let new_next = derivatives
+                .iter()
+                .rev()
+                .fold(0, |delta, prev| delta + prev.last().unwrap());
+
+            let new_prev = derivatives
+                .iter()
+                .rev()
+                .fold(0, |delta, prev| prev.first().unwrap() - delta);
+
+            #[cfg(debug_assertions)]
+            {
+                println!("previous value was {new_prev}, next value is {new_next}");
+                println!();
+            }
+
+            (next + new_next, prev + new_prev)
+        })
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (1939607039, 1041));
+    });
+}
+
+#[bench]
+fn bench_solve_without_original_clones_and_lazy_parse(b: &mut test::Bencher) {
+    #[allow(dead_code)]
+    fn solve(input: &str) -> (i32, i32) {
+        input
+            .lines()
+            .enumerate()
+            .map(|(line_no, line)| {
+                line.split_whitespace()
+                    .map(str::parse::<i32>)
+                    .collect::<Result<Vec<i32>, _>>()
+                    .unwrap_or_else(|e| panic!("error on line {}: {}", line_no + 1, e))
+            })
+            .fold((0, 0), |(next, prev), history| {
+                let mut derivatives = vec![history];
+
+                loop {
+                    let derivative = derivatives
+                        .last()
+                        .unwrap()
+                        .iter()
+                        .as_slice()
+                        .windows(2)
+                        .map(|pair| pair[1] - pair[0])
+                        .collect::<Vec<_>>();
+
+                    let brk = derivative
+                        .iter()
+                        .as_slice()
+                        .windows(2)
+                        .all(|w| w[0] == w[1]);
+
+                    derivatives.push(derivative);
+
+                    if brk {
+                        break;
+                    }
+                }
+
+                #[cfg(debug_assertions)]
+                {
+                    for d in &derivatives {
+                        println!("{:?}", d);
+                    }
+                    println!(
+                        "solved {history:?} after {steps} derivations",
+                        history = derivatives.first().unwrap(),
+                        steps = derivatives.len()
+                    );
+                }
+
+                let new_next = derivatives
+                    .iter()
+                    .rev()
+                    .fold(0, |delta, prev| delta + prev.last().unwrap());
+
+                let new_prev = derivatives
+                    .iter()
+                    .rev()
+                    .fold(0, |delta, prev| prev.first().unwrap() - delta);
+
+                #[cfg(debug_assertions)]
+                {
+                    println!("previous value was {new_prev}, next value is {new_next}");
+                    println!();
+                }
+
+                (next + new_next, prev + new_prev)
+            })
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (1939607039, 1041));
+    });
+}
+
+#[bench]
+fn bench_solve_without_parse(b: &mut test::Bencher) {
+    #[allow(dead_code)]
+    fn solve(histories: &[Vec<i32>]) -> (i32, i32) {
+        histories.iter().fold((0, 0), |(next, prev), history| {
+            let mut derivatives = vec![history.clone()];
+
+            loop {
+                let derivative = derivatives
+                    .last()
+                    .unwrap()
+                    .iter()
+                    .as_slice()
+                    .windows(2)
+                    .map(|pair| pair[1] - pair[0])
+                    .collect::<Vec<_>>();
+
+                let brk = derivative
+                    .iter()
+                    .as_slice()
+                    .windows(2)
+                    .all(|w| w[0] == w[1]);
+
+                derivatives.push(derivative);
+
+                if brk {
+                    break;
+                }
+            }
+
+            #[cfg(debug_assertions)]
+            {
+                for d in &derivatives {
+                    println!("{:?}", d);
+                }
+                println!(
+                    "solved {history:?} after {steps} derivations",
+                    history = derivatives.first().unwrap(),
+                    steps = derivatives.len()
+                );
+            }
+
+            let new_next = derivatives
+                .iter()
+                .rev()
+                .fold(0, |delta, prev| delta + prev.last().unwrap());
+
+            let new_prev = derivatives
+                .iter()
+                .rev()
+                .fold(0, |delta, prev| prev.first().unwrap() - delta);
+
+            #[cfg(debug_assertions)]
+            {
+                println!("previous value was {new_prev}, next value is {new_next}");
+                println!();
+            }
+
+            (next + new_next, prev + new_prev)
+        })
+    }
+
+    let histories = parse(INPUT).expect("couldn't parse input");
+
+    b.iter(|| {
+        assert_eq!(solve(&histories), (1939607039, 1041));
+    });
+}
