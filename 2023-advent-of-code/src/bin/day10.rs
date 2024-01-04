31bf773429b20bb1774bc8b672604b83b0fe278d
Ethan Ransom <derethanhausen@gmail.com>
2024-01-04 11:59:02 -0700

    aoc23: day10: part 2 using scanline method

--- 2023-advent-of-code/src/bin/day10.rs
+++ 2023-advent-of-code/src/bin/day10.rs
@@ -1,3 +1,4 @@
+#![feature(inline_const)]
 // #![feature(test)]
 
 // extern crate test;
@@ -56,86 +57,71 @@ fn solve(input: &str) -> (i64, i64) {
     let &max = visited.values().max().expect("no pipes reachable");
 
     // PART 2
-    let main_loop: HashSet<(i64, i64)> = visited.into_keys().collect();
+    let map = main_loop_map;
+    let mut inside = HashSet::new();
 
-    println!("BOUNDS: {:?}", main_loop_map.bounds);
+    let ((x_min, x_max), (y_min, y_max)) = map.bounds;
 
-    // BFS part two, although DFS would probably also work fine
-    let mut queue = VecDeque::new();
-    queue.push_back(map.start);
-    let mut visited = main_loop.clone();
-    let mut interiors = vec![];
-
-    for pos in &main_loop {
-        let neighbors = [
-            (pos.0, pos.1 - 1), // never
-            (pos.0 + 1, pos.1), // eat
-            (pos.0, pos.1 + 1), // soggy
-            (pos.0 - 1, pos.1), // waffles
-        ];
-        for neighbor in neighbors {
-            if !map.in_bounds(neighbor) {
-                continue;
-            }
-            if visited.contains(&neighbor) {
-                continue;
-            }
-
-            // 'flood'
-            let mut outside = false;
-            let mut seen = HashSet::new();
-            seen.insert(neighbor);
-            let mut queue = VecDeque::new();
-            queue.push_back(neighbor);
-
-            while let Some(pos) = queue.pop_front() {
-                let neighbors = [
-                    (pos.0, pos.1 - 1), // never
-                    (pos.0 + 1, pos.1), // eat
-                    (pos.0, pos.1 + 1), // soggy
-                    (pos.0 - 1, pos.1), // waffles
-                ];
-                for neighbor in neighbors {
-                    if !map.in_bounds(neighbor) {
-                        outside = true;
-                        continue;
+    for y in y_min..=y_max {
+        let mut in_region = false;
+        let [mut line, mut r, mut i] = [const { String::new() }; 3];
+        for x in x_min..=x_max {
+            let pos = (x, y);
+            match map.directions(pos) {
+                // |
+                [true, false, true, false] => in_region = !in_region,
+                // -
+                [false, true, false, true] => {}
+                // L
+                [true, true, false, false] => {}
+                // J
+                [true, false, false, true] => {}
+                // 7
+                [false, false, true, true] => in_region = !in_region,
+                // F
+                [false, true, true, false] => in_region = !in_region,
+                // .
+                [false, false, false, false] => {
+                    if in_region {
+                        inside.insert(pos);
                     }
-                    if visited.contains(&neighbor) {
-                        continue;
-                    }
-                    if !seen.insert(neighbor) {
-                        continue;
-                    }
-                    queue.push_back(neighbor);
                 }
-            }
 
-            visited.extend(&seen);
-            if !outside {
-                interiors.push(seen);
-            }
+                other => panic!("unknown neighbors: {:?}", other),
+            };
+            let c = if map.start == pos {
+                'S'
+            } else {
+                match map.directions(pos) {
+                    // | is a vertical pipe connecting north and south.
+                    [true, false, true, false] => '|',
+                    // - is a horizontal pipe connecting east and west.
+                    [false, true, false, true] => '-',
+                    // L is a 90-degree bend connecting north and east.
+                    [true, true, false, false] => 'L',
+                    // J is a 90-degree bend connecting north and west.
+                    [true, false, false, true] => 'J',
+                    // 7 is a 90-degree bend connecting south and west.
+                    [false, false, true, true] => '7',
+                    // F is a 90-degree bend connecting south and east.
+                    [false, true, true, false] => 'F',
+                    // . is ground; there is no pipe in this tile.
+                    [false, false, false, false] => '.',
+
+                    _ => panic!("unknown neighbors: {:?}", map.directions(pos)),
+                }
+            };
+            line.push(c);
+            r.push(if in_region { 'I' } else { 'O' });
+            i.push(if inside.contains(&pos) { '*' } else { ' ' });
         }
+        println!("{line}\n{r}\n{i}\n"); // NOTE: double newline
     }
 
-    let inside = interiors.iter().map(|i| i.len()).sum::<usize>();
-    println!("nodes on inside: {:?}", inside);
+    println!("{}", map.serialize_inside_fancy(&inside));
+    println!("enclosed area: {inside}", inside = inside.len());
 
-    println!(
-        "{}",
-        main_loop_map.serialize_inside_fancy(
-            &visited,
-            &main_loop,
-            &interiors
-                .into_iter()
-                .reduce(|mut a, b| {
-                    a.extend(b);
-                    a
-                })
-                .unwrap()
-        )
-    );
-
-    (max, inside as i64)
+    (max, inside.len() as i64)
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -146,14 +132,6 @@ struct Map {
 }
 
 impl Map {
-    fn in_bounds(&self, (x, y): (i64, i64)) -> bool {
-        let ((x_min, x_max), (y_min, y_max)) = self.bounds;
-
-        // dbg!((x, y));
-
-        x >= x_min && x <= x_max && y >= y_min && y <= y_max
-    }
-
     fn neighbors(&self, pos: (i64, i64)) -> Vec<(i64, i64)> {
         [
             (pos.0, pos.1 - 1), // never
@@ -258,12 +236,7 @@ impl Map {
         buf
     }
 
-    fn serialize_inside_fancy(
-        &self,
-        outside: &HashSet<(i64, i64)>,
-        main_loop: &HashSet<(i64, i64)>,
-        interiors: &HashSet<(i64, i64)>,
-    ) -> String {
+    fn serialize_inside_fancy(&self, interiors: &HashSet<(i64, i64)>) -> String {
         let mut buf = String::new();
 
         let ((x_min, x_max), (y_min, y_max)) = self.bounds;
@@ -273,12 +246,8 @@ impl Map {
             }
             for x in x_min..=x_max {
                 let pos = (x, y);
-                let c = if main_loop.contains(&pos) {
-                    '█'
-                } else if interiors.contains(&pos) {
-                    '*'
-                } else if outside.contains(&pos) {
-                    '░'
+                let c = if interiors.contains(&pos) {
+                    'I'
                 } else if self.start == pos {
                     'S'
                 } else {
@@ -398,12 +367,81 @@ fn test_parse_unparse() {
 
 #[test]
 fn test_example() {
-    assert_eq!(solve(EXAMPLE), (8, 0));
+    assert_eq!(solve(EXAMPLE), (8, 1));
+}
+
+#[test]
+fn test_part2_examples() {
+    assert_eq!(
+        solve(
+            "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."
+        )
+        .1,
+        4
+    );
+
+    assert_eq!(
+        solve(
+            "..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+.........."
+        )
+        .1,
+        4
+    );
+
+    assert_eq!(
+        solve(
+            ".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ..."
+        )
+        .1,
+        8
+    );
+
+    assert_eq!(
+        solve(
+            "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"
+        )
+        .1,
+        10
+    );
 }
 
 #[test]
 fn test_input() {
-    assert_eq!(solve(INPUT), (7102, 0));
+    assert_eq!(solve(INPUT), (7102, 363));
 }
 
 // #[bench]
