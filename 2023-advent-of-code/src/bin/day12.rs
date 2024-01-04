22a876eef2a99ffd6bfcb9b187272138bed2a737
Ethan Ransom <derethanhausen@gmail.com>
2024-01-04 14:06:15 -0700

    aoc23: day12: almost twice as fast

--- 2023-advent-of-code/src/bin/day12.rs
+++ 2023-advent-of-code/src/bin/day12.rs
@@ -43,58 +43,44 @@ fn sum_possibilities(rows: &Vec<(String, Vec<usize>)>) -> usize {
 
     for (springs, actual_groups) in rows {
         let mut count = 0;
-        // println!("{springs}:");
-        let ps = possibilities(springs);
-        for p in &ps {
-            let groups = get_groups(p);
-            // println!("\t{p}\t{groups:?}\t{actual_groups:?}");
-            if &groups == actual_groups {
-                count += 1;
-            }
-        }
+        println!("{springs}: {actual_groups:?}");
+        possibilities(springs, actual_groups, &mut count);
         // println!("  -> {count}");
         sum += count;
     }
     sum
 }
 
-fn possibilities(springs: &str) -> Vec<String> {
-    if springs.is_empty() {
-        return vec![];
+fn possibilities(springs: &str, actual_groups: &[usize], count: &mut usize) {
+    fn possibilities(
+        original_springs: &str,
+        springs: &mut str,
+        pos: usize,
+        actual_groups: &[usize],
+        count: &mut usize,
+    ) {
+        // println!("possibilities {springs} {pos}");
+        if pos >= original_springs.len() {
+            // println!("is_empty {springs}");
+            if get_groups(springs) == actual_groups {
+                *count += 1;
+            }
+            return;
+        }
+
+        if original_springs.as_bytes()[pos] == b'?' {
+            unsafe { springs.as_bytes_mut()[pos] = b'#' };
+            possibilities(original_springs, springs, pos + 1, actual_groups, count);
+            unsafe { springs.as_bytes_mut()[pos] = b'.' };
+            possibilities(original_springs, springs, pos + 1, actual_groups, count);
+        } else {
+            possibilities(original_springs, springs, pos + 1, actual_groups, count);
+        }
     }
-    let (c, springs) = (&springs[..1], &springs[1..]);
-    // println!("\t{c:?} {springs:?}");
-    let mut ps = possibilities(springs);
-    if c == "?" {
-        ["#", "."]
-            .iter()
-            .flat_map(|c| {
-                let mut ps = ps.clone();
-                if ps.is_empty() {
-                    ps.push(c.to_string());
-                    ps
-                } else {
-                    ps.into_iter()
-                        .map(|mut p| {
-                            p.insert_str(0, c);
-                            p
-                        })
-                        .collect()
-                }
-            })
-            .collect()
-    } else if ps.is_empty() {
-        ps.push(c.to_string());
-        ps
-    } else {
-        ps.into_iter()
-            // .inspect(|p| println!("\t-> {p}"))
-            .map(|mut p| {
-                p.insert_str(0, c);
-                p
-            })
-            .collect()
-    }
+
+    let mut working_copy = springs.to_owned();
+
+    possibilities(springs, &mut working_copy, 0, actual_groups, count);
 }
 
 fn get_groups(springs: &str) -> Vec<usize> {
@@ -134,7 +120,87 @@ fn test_input() {
 }
 
 #[bench]
-fn bench_solve_naive_example(b: &mut test::Bencher) {
+fn bench_solve_example_current(b: &mut test::Bencher) {
+    // TODO idk could test different allocation strategies here
+    b.iter(|| {
+        let rows = parse(EXAMPLE);
+        assert_eq!(sum_possibilities(&rows), 21);
+    });
+}
+
+#[bench]
+fn bench_solve_example_original(b: &mut test::Bencher) {
+    #[allow(dead_code)]
+    fn sum_possibilities(rows: &Vec<(String, Vec<usize>)>) -> usize {
+        let mut sum = 0;
+
+        for (springs, actual_groups) in rows {
+            let mut count = 0;
+            // println!("{springs}:");
+            let ps = possibilities(springs);
+            for p in &ps {
+                let groups = get_groups(p);
+                // println!("\t{p}\t{groups:?}\t{actual_groups:?}");
+                if &groups == actual_groups {
+                    count += 1;
+                }
+            }
+            // println!("  -> {count}");
+            sum += count;
+        }
+        sum
+    }
+
+    #[allow(dead_code)]
+    fn possibilities(springs: &str) -> Vec<String> {
+        if springs.is_empty() {
+            return vec![];
+        }
+        let (c, springs) = (&springs[..1], &springs[1..]);
+        // println!("\t{c:?} {springs:?}");
+        let mut ps = possibilities(springs);
+        if c == "?" {
+            ["#", "."]
+                .iter()
+                .flat_map(|c| {
+                    let mut ps = ps.clone();
+                    if ps.is_empty() {
+                        ps.push(c.to_string());
+                        ps
+                    } else {
+                        ps.into_iter()
+                            .map(|mut p| {
+                                p.insert_str(0, c);
+                                p
+                            })
+                            .collect()
+                    }
+                })
+                .collect()
+        } else if ps.is_empty() {
+            ps.push(c.to_string());
+            ps
+        } else {
+            ps.into_iter()
+                // .inspect(|p| println!("\t-> {p}"))
+                .map(|mut p| {
+                    p.insert_str(0, c);
+                    p
+                })
+                .collect()
+        }
+    }
+
+    #[allow(dead_code)]
+    fn get_groups(springs: &str) -> Vec<usize> {
+        springs
+            .chars()
+            .group_by(|&c| c)
+            .into_iter()
+            .filter_map(|(c, group)| if c == '#' { Some(group.count()) } else { None })
+            .collect()
+    }
+
     // TODO idk could test different allocation strategies here
     b.iter(|| {
         let rows = parse(EXAMPLE);
