62ff850f9e3b4337c53833725e5ed48a53aeb523
Ethan Ransom <derethanhausen@gmail.com>
2024-12-01 00:05:32 -0700

    aoc22: day01: benchys for days

--- 2024-advent-of-code/src/bin/day01.rs
+++ 2024-advent-of-code/src/bin/day01.rs
@@ -1,6 +1,6 @@
-// #![feature(test)]
+#![feature(test)]
 
-// extern crate test;
+extern crate test;
 
 use core::iter::Iterator;
 
@@ -39,8 +39,6 @@ fn parse(input: &str) -> Vec<(u32, u32)> {
         .map(|line| {
             let (left, right) = line.split_once("   ").unwrap();
 
-            dbg!(left, right);
-
             (left.parse().unwrap(), right.parse().unwrap())
         })
         .collect()
@@ -56,9 +54,200 @@ fn test_input() {
     assert_eq!(solve(INPUT), (3508942, 26593248));
 }
 
-// #[bench]
-// fn bench_solve_current(b: &mut test::Bencher) {
-//     b.iter(|| {
-//         assert_eq!(solve(INPUT), (0, 0));
-//     });
-// }
+#[bench]
+fn bench_solve_01_current(b: &mut test::Bencher) {
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (3508942, 26593248));
+    });
+}
+
+#[bench]
+fn bench_solve_02_heap_and_hashcounter(b: &mut test::Bencher) {
+    type BinaryHeap = std::collections::BinaryHeap<u32>;
+
+    use std::collections::HashMap;
+
+    fn solve(input: &str) -> (usize, usize) {
+        let input = input.trim();
+
+        let (mut left, mut right) = (BinaryHeap::new(), BinaryHeap::new());
+
+        let mut right_freqs = HashMap::<u32, usize>::new();
+
+        for line in input.lines() {
+            let (a, b) = line.split_once("   ").unwrap();
+
+            let [a, b] = [a, b].map(|v| v.parse().unwrap());
+
+            left.push(a);
+            right.push(b);
+
+            right_freqs
+                .entry(b)
+                .and_modify(|counter| *counter += 1)
+                .or_insert(1);
+        }
+
+        let [left, right] = [left, right].map(|v| v.into_sorted_vec());
+
+        (
+            left.iter()
+                .zip(right.iter())
+                .map(|(&left, &right)| left.abs_diff(right) as usize)
+                .sum::<usize>(),
+            left.iter()
+                .map(|&n| right_freqs.get(&n).unwrap_or(&0) * n as usize)
+                .sum(),
+        )
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (3508942, 26593248));
+    });
+}
+
+#[bench]
+fn bench_solve_03_just_hashcounter(b: &mut test::Bencher) {
+    use std::collections::HashMap;
+
+    fn solve(input: &str) -> (usize, usize) {
+        let input = input.trim();
+
+        let (mut left, mut right) = (Vec::new(), Vec::new());
+
+        let mut right_freqs = HashMap::<u32, usize>::new();
+
+        for line in input.lines() {
+            let (a, b) = line.split_once("   ").unwrap();
+
+            let [a, b] = [a, b].map(|v| v.parse().unwrap());
+
+            left.push(a);
+            right.push(b);
+
+            right_freqs
+                .entry(b)
+                .and_modify(|counter| *counter += 1)
+                .or_insert(1);
+        }
+
+        left.sort();
+        right.sort();
+
+        (
+            left.iter()
+                .zip(right.iter())
+                .map(|(&left, &right)| left.abs_diff(right) as usize)
+                .sum::<usize>(),
+            left.iter()
+                .map(|&n| right_freqs.get(&n).unwrap_or(&0) * n as usize)
+                .sum(),
+        )
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (3508942, 26593248));
+    });
+}
+
+#[bench]
+fn bench_solve_04_fxhash_hashcounter(b: &mut test::Bencher) {
+    use fxhash::FxHashMap as HashMap;
+
+    fn solve(input: &str) -> (usize, usize) {
+        let input = input.trim();
+
+        let (mut left, mut right) = (Vec::new(), Vec::new());
+
+        let mut right_freqs = HashMap::<u32, usize>::default();
+
+        for line in input.lines() {
+            let (a, b) = line.split_once("   ").unwrap();
+
+            let [a, b] = [a, b].map(|v| v.parse().unwrap());
+
+            left.push(a);
+            right.push(b);
+
+            right_freqs
+                .entry(b)
+                .and_modify(|counter| *counter += 1)
+                .or_insert(1);
+        }
+
+        left.sort();
+        right.sort();
+
+        (
+            left.iter()
+                .zip(right.iter())
+                .map(|(&left, &right)| left.abs_diff(right) as usize)
+                .sum::<usize>(),
+            left.iter()
+                .map(|&n| right_freqs.get(&n).unwrap_or(&0) * n as usize)
+                .sum(),
+        )
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (3508942, 26593248));
+    });
+}
+
+#[bench]
+fn bench_solve_part1_01_current(b: &mut test::Bencher) {
+    fn solve(input: &str) -> (usize, usize) {
+        let input = parse(input);
+
+        let (mut left, mut right): (Vec<u32>, Vec<u32>) = input.into_iter().unzip();
+
+        left.sort();
+        right.sort();
+
+        (
+            left.iter()
+                .zip(right.iter())
+                .map(|(&left, &right)| left.abs_diff(right) as usize)
+                .sum::<usize>(),
+            0,
+        )
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (3508942, 0));
+    });
+}
+
+#[bench]
+fn bench_solve_part1_02_heap(b: &mut test::Bencher) {
+    type BinaryHeap = std::collections::BinaryHeap<u32>;
+
+    fn solve(input: &str) -> (usize, usize) {
+        let input = input.trim();
+
+        let (mut left, mut right) = (BinaryHeap::new(), BinaryHeap::new());
+
+        for line in input.lines() {
+            let (a, b) = line.split_once("   ").unwrap();
+
+            let [a, b] = [a, b].map(|v| v.parse().unwrap());
+
+            left.push(a);
+            right.push(b);
+        }
+
+        let [left, right] = [left, right].map(|v| v.into_sorted_vec());
+
+        (
+            left.iter()
+                .zip(right.iter())
+                .map(|(&left, &right)| left.abs_diff(right) as usize)
+                .sum::<usize>(),
+            0,
+        )
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (3508942, 0));
+    });
+}
