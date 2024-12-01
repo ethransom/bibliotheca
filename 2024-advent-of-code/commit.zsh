8a9bd2bc652ca55015f711f259b51e8d5825aa29
Ethan Ransom <derethanhausen@gmail.com>
2024-11-30 22:23:53 -0700

    aoc24: first!!!!!!

---
+++ 2024-advent-of-code/commit.zsh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# exit on all non-zero codes
+set -e
+
+# precommit checks
+cargo test
+cargo clippy
+
+day=$(git status . --porcelain | (rg 'src/bin/(day\d\d).rs' -or '$1' || true))
+
+if [[ -z "${day// }" ]]; then
+    echo 'WARNING: NO DAY DETECTED'
+    day=""
+elif [ $(echo $day | wc -l) = "1" ]; then
+    echo "DETECTED $day"
+    day=" $day:"
+else
+    echo 'WARNING: MULTIPLE DAYS DETECTED'
+    day=""
+fi
+
+git add .
+
+echo 'COMMIT MESSAGE: (Ctrl+C to abort)'
+git commit -m "aoc22:$day $(read -r; printf '%s' "$REPLY")"
