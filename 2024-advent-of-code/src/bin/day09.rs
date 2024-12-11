3ac2b5642a172d384acdcdfc6c3158c9d3fed350
Ethan Ransom <derethanhausen@gmail.com>
2024-12-10 18:59:44 -0700

    aoc24: day09: part1

---
+++ 2024-advent-of-code/src/bin/day09.rs
@@ -0,0 +1,90 @@
+// #![feature(test)]
+
+// extern crate test;
+
+const EXAMPLE: &str = include_str!("example09.txt");
+const INPUT: &str = include_str!("input09.txt");
+
+fn main() {
+    dbg!(solve(EXAMPLE));
+    dbg!(solve(INPUT));
+}
+
+fn solve(input: &str) -> (usize, usize) {
+    let blocks = parse(input);
+
+    let mut disk = Vec::<Option<usize>>::new();
+    let mut id = 0;
+    for (i, len) in blocks.iter().enumerate() {
+        let block = i % 2 == 0;
+        if block {
+            for _ in 0..*len {
+                disk.push(Some(id));
+            }
+            id += 1;
+        } else {
+            for _ in 0..*len {
+                disk.push(None);
+            }
+        }
+    }
+
+    // println!("DD: {:?}", disk);
+
+    let mut i = 0;
+    let mut len = disk.len();
+    loop {
+        if i >= len {
+            break;
+        }
+        if disk[i].is_none() {
+            while disk[len - 1].is_none() {
+                len -= 1;
+                if i >= len - 1 {
+                    break;
+                }
+            }
+
+            disk.swap(i, len - 1);
+        }
+        // for v in disk.iter() {
+        //     let c = if v.is_some() { 'X' } else { '.' };
+        //     print!("{c}");
+        // }
+        // println!("");
+        i += 1;
+    }
+
+    (
+        disk.iter()
+            .enumerate()
+            .filter_map(|(i, c)| c.map(|c| i * c))
+            .sum(),
+        0,
+    )
+}
+
+fn parse(input: &str) -> Vec<u8> {
+    input.trim().bytes().map(btoi).collect::<Vec<_>>()
+}
+
+fn btoi(b: u8) -> u8 {
+    b - b'0'
+}
+
+#[test]
+fn test_example() {
+    assert_eq!(solve(EXAMPLE), (1928, 0));
+}
+
+#[test]
+fn test_input() {
+    assert_eq!(solve(INPUT), (6607511583593, 0));
+}
+
+// #[bench]
+// fn bench_solve_current(b: &mut test::Bencher) {
+//     b.iter(|| {
+//         assert_eq!(solve(INPUT), (0, 0));
+//     });
+// }
