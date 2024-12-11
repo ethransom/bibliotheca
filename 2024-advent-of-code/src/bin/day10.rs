9c008fdac3356c38e0da4658b238b36b8b4e09f9
Ethan Ransom <derethanhausen@gmail.com>
2024-12-10 20:15:18 -0700

    aoc24: day10: benches! (fxhash 2x omfg)

--- 2024-advent-of-code/src/bin/day10.rs
+++ 2024-advent-of-code/src/bin/day10.rs
@@ -27,7 +27,7 @@ fn solve(input: &str) -> (usize, usize) {
         .filter(|&(_pos, h)| *h == 9)
         .map(|(pos, _h)| *pos);
 
-    println!("{s:?}", s = starts.clone().collect::<Vec<_>>());
+    // println!("{s:?}", s = starts.clone().collect::<Vec<_>>());
 
     let mut trailhead_scores = HashMap::<Point, HashMap<Point, usize>>::new();
 
@@ -74,10 +74,12 @@ fn solve(input: &str) -> (usize, usize) {
     // }
 
     (
+        // number of paths unique by starting point
         trailhead_scores
             .values()
             .map(|m| m.keys().collect::<HashSet<_>>().len())
             .sum(),
+        // number of paths summed by starting point
         trailhead_scores.values().flat_map(|m| m.values()).sum(),
     )
 }
@@ -134,8 +136,96 @@ fn test_input() {
 }
 
 #[bench]
-fn bench_solve_current(b: &mut test::Bencher) {
+fn bench_solve_00_current(b: &mut test::Bencher) {
     b.iter(|| {
         test_input();
+    });
+}
+
+#[bench]
+fn bench_solve_01_current_fxhash(b: &mut test::Bencher) {
+    use fxhash::{FxHashMap as HashMap, FxHashSet as HashSet};
+
+    fn solve(input: &str) -> (usize, usize) {
+        let map = parse(input);
+
+        // println!("{map}");
+
+        let starts = map
+            .map
+            .iter()
+            .filter(|&(_pos, h)| *h == 9)
+            .map(|(pos, _h)| *pos);
+
+        let mut trailhead_scores = HashMap::<Point, HashMap<Point, usize>>::default();
+
+        for start in starts {
+            let mut visited = HashSet::<Point>::default();
+            visited.insert(start);
+            let mut stack = vec![(start, visited)];
+
+            while let Some(((x, y), visited)) = stack.pop() {
+                let &height = map.map.get(&(x, y)).unwrap();
+
+                if height == 0 {
+                    // valid trailhead
+                    trailhead_scores
+                        .entry((x, y))
+                        .or_default()
+                        .entry(start)
+                        .and_modify(|c| *c += 1)
+                        .or_insert(1);
+                }
+
+                for (dx, dy) in NEIGHBORS {
+                    let neighbor = (x + dx, y + dy);
+                    let Some(neighbor_height) = map.map.get(&neighbor) else {
+                        continue;
+                    };
+
+                    if neighbor_height + 1 == height && !visited.contains(&neighbor) {
+                        let visited = visited.clone();
+                        stack.push((neighbor, visited));
+                    }
+                }
+            }
+        }
+
+        (
+            trailhead_scores
+                .values()
+                .map(|m| m.keys().collect::<HashSet<_>>().len())
+                .sum(),
+            trailhead_scores.values().flat_map(|m| m.values()).sum(),
+        )
+    }
+
+    #[derive(Clone)]
+    struct Map {
+        map: HashMap<Point, u8>,
+    }
+
+    fn parse(input: &str) -> Map {
+        let mut map = HashMap::default();
+
+        let mut height = 0;
+        let mut width = 0;
+
+        for (y, line) in input.lines().enumerate() {
+            height = height.max(y);
+            for (x, c) in line.chars().enumerate() {
+                width = width.max(x);
+                map.insert(
+                    (x as isize, y as isize),
+                    c.to_digit(10).unwrap().try_into().unwrap(),
+                );
+            }
+        }
+
+        Map { map }
+    }
+
+    b.iter(|| {
+        assert_eq!(solve(INPUT), (611, 1380));
     });
 }
