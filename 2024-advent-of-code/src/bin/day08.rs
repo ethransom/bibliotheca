d4ac27b33ab34a52185247b918a1d0cc9546b10d
Ethan Ransom <derethanhausen@gmail.com>
2024-12-14 21:27:49 -0700

    aoc24: day08: part 2

--- 2024-advent-of-code/src/bin/day08.rs
+++ 2024-advent-of-code/src/bin/day08.rs
@@ -22,50 +22,71 @@ fn solve(input: &str) -> (usize, usize) {
 
     let mut antinodes = HashSet::<Point>::new();
 
-    for (node, locs) in nodes {
-        let new = find_antinodes(locs.clone());
-        for y in 0..=height {
-            for x in 0..=width {
-                let (x, y) = (x as isize, y as isize);
-                let c = if new.contains(&(x, y)) {
-                    '#'
-                } else if locs.contains(&(x, y)) {
-                    node
-                } else {
-                    '.'
-                };
-                print!("{c}");
+    for (_node, locs) in nodes.clone() {
+        let new = {
+            let locs = locs.clone();
+            let mut antinodes = vec![];
+
+            for (a, b) in locs.iter().tuple_combinations() {
+                let &(ax, ay) = a;
+                let &(bx, by) = b;
+
+                let antinode = (ax + (ax - bx), ay + (ay - by));
+                antinodes.push(antinode);
+
+                let antinode = (bx + (bx - ax), by + (by - ay));
+
+                antinodes.push(antinode);
             }
-            println!("");
-        }
+
+            antinodes
+        };
+        // for y in 0..=height {
+        //     for x in 0..=width {
+        //         let (x, y) = (x as isize, y as isize);
+        //         let c = if new.contains(&(x, y)) {
+        //             '#'
+        //         } else if locs.contains(&(x, y)) {
+        //             node
+        //         } else {
+        //             '.'
+        //         };
+        //         print!("{c}");
+        //     }
+        //     println!("");
+        // }
         antinodes = antinodes.into_iter().chain(new.into_iter()).collect();
+    }
+
+    // get all node pair combos
+    let lines: Vec<(Point, Point)> = nodes
+        .iter()
+        .flat_map(|(_c, locs)| locs.clone().into_iter().tuple_combinations::<(_, _)>())
+        .collect();
+
+    let mut part2_antinodes = HashSet::<Point>::new();
+
+    for y in 0..=height {
+        for x in 0..=width {
+            let (x, y) = (x as isize, y as isize);
+
+            let is_antinode = lines
+                .iter()
+                .any(|&((x1, y1), (x2, y2))| (x2 - x1) * (y - y1) - (y2 - y1) * (x - x1) == 0);
+
+            if is_antinode {
+                part2_antinodes.insert((x, y));
+            }
+
+            // let c = if is_antinode { '#' } else { '.' };
+            // print!("{c}");
+        }
+        // println!("");
     }
 
     antinodes.retain(|&(x, y)| x >= 0 && y >= 0 && x <= width as isize && y <= height as isize);
 
-    (antinodes.len(), 0)
-}
-
-fn find_antinodes(locs: Vec<Point>) -> Vec<Point> {
-    let mut antinodes = vec![];
-
-    for (a, b) in locs.iter().tuple_combinations() {
-        println!("\tcombo {a:?} {b:?}");
-
-        let &(ax, ay) = a;
-        let &(bx, by) = b;
-
-        let antinode = (ax + (ax - bx), ay + (ay - by));
-        println!("\t\t{a:?} {b:?}: {antinode:?}");
-        antinodes.push(antinode);
-
-        let antinode = (bx + (bx - ax), by + (by - ay));
-        println!("\t\t{a:?} {b:?}: {antinode:?}");
-
-        antinodes.push(antinode);
-    }
-
-    antinodes
+    (antinodes.len(), part2_antinodes.len())
 }
 
 fn parse(input: &str) -> (HashMap<char, Vec<Point>>, usize, usize) {
@@ -92,45 +113,61 @@ fn parse(input: &str) -> (HashMap<char, Vec<Point>>, usize, usize) {
 fn test_small_example() {
     assert_eq!(
         solve(
-            "..........
-    ..........
-    ..........
-    ....a.....
-    ..........
-    .....a....
-    ..........
-    ..........
-    ..........
-    .........."
+            "T.........
+            ...T......
+            .T........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            .........."
         ),
-        (2, 0)
+        (3, 9)
     );
 
     assert_eq!(
         solve(
             "..........
-    ..........
-    ..........
-    ....a.....
-    ........a.
-    .....a....
-    ..........
-    ..........
-    ..........
-    .........."
+            ..........
+            ..........
+            ....a.....
+            ..........
+            .....a....
+            ..........
+            ..........
+            ..........
+            .........."
         ),
-        (4, 0)
+        (2, 5)
+    );
+
+    assert_eq!(
+        solve(
+            "..........
+            ..........
+            ..........
+            ....a.....
+            ........a.
+            .....a....
+            ..........
+            ..........
+            ..........
+            .........."
+        ),
+        (4, 8)
     );
 }
 
 #[test]
 fn test_example() {
-    assert_eq!(solve(EXAMPLE), (14, 0));
+    assert_eq!(solve(EXAMPLE), (14, 34));
 }
 
 #[test]
 fn test_input() {
-    assert_eq!(solve(INPUT), (0, 0));
+    assert_eq!(solve(INPUT), (293, 934));
 }
 
 // #[bench]
